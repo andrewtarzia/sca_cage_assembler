f4063576ba53496f360fbc6b06db93bf5fd09de1
Andrew <andrew.tarzia@gmail.com>
2019-03-15 09:28:09 +0000

    first commit of tim+irene tests

---
+++ irene_ff/ASE-UFF-LAMMPs/uff.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Distributed under the terms of the MIT License.
+
+"""
+Module with functions for using the UFF4MOF in Lammps
+
+Author: Andrew Tarzia
+
+Date Created: 19 Feb 2019
+
+"""
