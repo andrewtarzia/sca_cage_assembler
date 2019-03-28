237b83d72f73a3090922a147bae49b0315cdf907
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-03-28 12:16:32 +0000

    add histogram function, minor fixes

--- plotting.py
+++ plotting.py
@@ -21,10 +21,9 @@ def parity_plot(X, Y, outfile, xtitle, ytitle, lim):
     fig, ax = plt.subplots(figsize=(5, 5))
     ax.scatter(X, Y, c='firebrick', edgecolors='k',
                marker='o', alpha=1.0, s=80)
-    ax.plot(np.linspace(-1, max(lim)+1, 2),
-            np.linspace(-1, max(lim)+1, 2),
+    ax.plot(np.linspace(min(lim) - 1, max(lim) + 1, 2),
+            np.linspace(min(lim) - 1, max(lim) + 1, 2),
             c='k', alpha=0.4)
-
     # Set number of ticks for x-axis
     ax.tick_params(axis='both', which='major', labelsize=16)
     ax.set_xlabel(xtitle, fontsize=16)
@@ -34,3 +33,35 @@ def parity_plot(X, Y, outfile, xtitle, ytitle, lim):
     fig.tight_layout()
     fig.savefig(outfile, dpi=720,
                 bbox_inches='tight')
+
+
+def histogram_plot_1(Y, X_range, width, alpha, color, edgecolor,
+                     outfile, xtitle, density=False):
+    '''Make histogram plot with 1 distribution.
+
+    '''
+
+    fig, ax = plt.subplots(figsize=(8, 5))
+    X_bins = np.arange(X_range[0], X_range[1], width)
+    hist, bin_edges = np.histogram(a=Y, bins=X_bins, density=density)
+    ax.bar(bin_edges[:-1],
+           hist,
+           align='edge',
+           alpha=alpha, width=width,
+           color=color,
+           edgecolor=edgecolor)
+    ax.tick_params(axis='both', which='major', labelsize=16)
+    ax.set_xlabel(xtitle, fontsize=16)
+    if density is False:
+        ax.set_ylabel('count', fontsize=16)
+    elif density is True:
+        ax.set_ylabel('frequency', fontsize=16)
+    ax.set_xlim(X_range)
+    fig.tight_layout()
+    fig.savefig(outfile, dpi=720,
+                bbox_inches='tight')
+
+
+def flat_line(ax, x, y, w=0, C='k', m='x'):
+    ax.plot([x - w, x, x + w], [y, y, y], c=C)
+    ax.scatter(x, y, marker=m, c=C)
