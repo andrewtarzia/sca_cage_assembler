c503ebd11ccdd860b516d21b6e2e4ad03e6259f0
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-03-28 16:29:24 +0000

    minor

--- GFN_functions.py
+++ GFN_functions.py
@@ -22,13 +22,13 @@ def setup_dirs(xyzs):
     for i in xyzs:
         file = i.replace('.xyz', '')
         print(file)
-        os.system('mkdir '+file)
-        os.system('cp '+i+' '+file+'/')
+        os.system('mkdir ' + file)
+        os.system('cp ' + i + ' ' + file + '/')
         if os.path.isfile('xctrl') is False:
             print('copy an xctrl file in this dir!', os.getcwd())
             sys.exit('exitting.')
         else:
-            os.system('cp xctrl '+file+'/')
+            os.system('cp xctrl ' + file + '/')
 
 
 def get_energies(output_file):
@@ -46,30 +46,30 @@ def get_energies(output_file):
     for line in open(output_file, 'r'):
         # free energy in a.u.
         if 'TOTAL FREE ENERGY' in line:
-            l = line.rstrip().split('<===')
-            FE_au = l[0].strip()
+            li = line.rstrip().split('<===')
+            FE_au = li[0].strip()
             # print(FE_au)
             results_part['FE'] = FE_au
         if 'total E       :' in line:
-            l = line.rstrip().split(':')
-            TE_au = l[1].strip()
+            li = line.rstrip().split(':')
+            TE_au = li[1].strip()
             # print(TE_au)
             results_part['TE'] = TE_au
         if 'SCC energy    :' in line:
-            l = line.rstrip().split(':')
-            SCE_au = l[1].strip()
+            li = line.rstrip().split(':')
+            SCE_au = li[1].strip()
             # print(TE_au)
             results_part['SCE'] = SCE_au
         if 'G(T)' in line:
-            l = line.rstrip().split(' ')
-            new_l = [i for i in l if i != '']
+            li = line.rstrip().split(' ')
+            new_l = [i for i in li if i != '']
             # print(new_l)
             GT_au = new_l[1].strip()
             # print(GT_au)
             results_part['GT'] = GT_au
         if 'H(T)' in line and 'H(0)-H(T)+PV' not in line:
-            l = line.rstrip().split(' ')
-            new_l = [i for i in l if i != '']
+            li = line.rstrip().split(' ')
+            new_l = [i for i in li if i != '']
             # print(new_l)
             HT_au = new_l[1].strip()
             # print(HT_au)
