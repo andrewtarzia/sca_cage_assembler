c1bdca120a32007f5762f400aaa1cea96eda3f1e
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-04-04 11:06:07 +0100

    the first half of test_shape.py is used to extarct independant cages (using pywindow)

---
+++ cage_collect/extract_indep_cages.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Distributed under the terms of the MIT License.
+
+"""
+Script to extract all independant cages from a CIF using pyWindow code.
+
+Author: Andrew Tarzia
+
+Date Created: 04 Apr 2019
+
+"""
+
+import sys
+sys.path.insert(0, '/home/atarzia/thesource/')
+from pywindow_functions import (analyze_rebuilt, rebuild_system)
+from IO_tools import convert_CIF_2_PDB
+
+
+def main():
+    if (not len(sys.argv) == 2):
+        print("""
+Usage: extract_indep_cages.py CIF
+    CIF (str) - name of CIF to analyze
+    """)
+        sys.exit()
+    else:
+        CIF = sys.argv[1]
+
+    if CIF[-4:] != '.cif':
+        raise Exception('input file: {} was not a CIF'.format(CIF))
+
+    pdb_file, struct = convert_CIF_2_PDB(CIF)
+    rebuilt_structure = rebuild_system(file=pdb_file)
+    rebuilt_structure.make_modular()
+    res = analyze_rebuilt(rebuilt_structure, file_prefix=CIF.rstrip('.cif'),
+                          atom_limit=20, include_coms=False, verbose=False)
+    print('===================================================')
+    print('Results of pyWindow analysis on all indep cages:')
+    print('===================================================')
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
