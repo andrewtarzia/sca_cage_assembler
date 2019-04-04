41a411f91a2b5db93945261c0afba75400308c63
Andrew Tarzia <andrew.tarzia@adelaide.edu.au>
2019-04-04 16:18:08 +0100

    create molecule buidling script

---
+++ ligand_combiner/molecule_building.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Distributed under the terms of the MIT License.
+
+"""
+Script to build all molecules from DB of core, ligands and linkers.
+
+Author: Andrew Tarzia
+
+Date Created: 04 Apr 2019
+
+"""
+if __name__ == "__main__":
+    main()
