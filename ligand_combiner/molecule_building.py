06fa7430e3be5cabe2ef89ca99f514cd4f9fc643
Andrew Tarzia <andrew.tarzia@adelaide.edu.au>
2019-04-04 16:18:33 +0100

    build molecule/building block populations

--- ligand_combiner/molecule_building.py
+++ ligand_combiner/molecule_building.py
@@ -10,5 +10,33 @@ Author: Andrew Tarzia
 Date Created: 04 Apr 2019
 
 """
+
+import sys
+from os.path import join
+from stk import rdkit_ETKDG
+sys.path.insert(0, '/home/atarzia/thesource/')
+from stk_functions import build_population, build_ABCBA, build_ABA
+
+
+def main():
+    if (not len(sys.argv) == 1):
+        print("""
+    Usage: molecule_builing.py
+        """)
+        sys.exit()
+    else:
+        # CIF = sys.argv[1]
+        pass
+
+    proj_dir = '/home/atarzia/projects/ligand_combiner/'
+    core_dir = proj_dir + 'cores/'
+    liga_dir = proj_dir + 'ligands/'
+    link_dir = proj_dir + 'linkers/'
+    mole_dir = proj_dir + 'molecules/'
+
+    # build molecule populations
+    core_pop = build_population(directory=core_dir, fgs=['bromine'])
+    liga_pop = build_population(directory=liga_dir, fgs=['bromine'])
+    link_pop = build_population(directory=link_dir, fgs=['bromine'])
 if __name__ == "__main__":
     main()
