fda5a52df6ea1f864d10d03224e6cd036e09f79b
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-04-05 13:48:41 +0100

    refactored

--- ligand_combiner/molecule_building.py
+++ ligand_combiner/molecule_building.py
@@ -18,6 +18,54 @@ sys.path.insert(0, '/home/atarzia/thesource/')
 from stk_functions import build_population, build_ABCBA, build_ABA
 
 
+def get_molecule(type, popns, pop_ids, N=1, mole_dir='./'):
+    '''Get N conformers of a coordination cage ligand molecule using
+    stk polymer function. Molecule undergoes RDKIT ETKDG conformer search and
+    optimization with UFF.
+
+    Keyword Arguments:
+        type (str) - type of ligand, ABCBA or ABA
+        popns (list of stk.Populations) - core, ligand and linker
+            stk.Populations
+        pop_ids (tuple of int) - population indices to use from core, ligand and
+            linker populations
+        N (int) - number of conformers to build
+        mole_dir (str) - directory to save molecule to
+
+    Returns:
+        cids (list) - list of conformer IDs
+        molecule (stk.Polymer) - polymer molecule with N conformers
+            in molecule.mol
+
+    '''
+    core_item = popns[0][pop_ids[0]]
+    liga_item = popns[1][pop_ids[1]]
+    link_item = popns[2][pop_ids[2]]
+    if type == 'ABCBA':
+        molecule = build_ABCBA(core=core_item,
+                               liga=liga_item,
+                               link=link_item)
+        prefix = core_item.name + '_'
+        prefix += liga_item.name + '_'
+        prefix += link_item.name
+    elif type == 'ABA':
+        molecule = build_ABA(core=core_item,
+                             liga=liga_item)
+        prefix = core_item.name + '_'
+        prefix += liga_item.name
+
+    # output as built
+    json_file = prefix + '_' + type + '.json'
+    molecule.dump(join(mole_dir, json_file))
+    mol_file = prefix + '_' + type + '.mol'
+    molecule.write(join(mole_dir, mol_file))
+    # clean molecule
+    rdkit_ETKDG(molecule)
+    # output energy minimized
+    json_file = prefix + '_' + type + '_opt.json'
+    molecule.dump(join(mole_dir, json_file))
+    mol_file = prefix + '_' + type + '_opt.mol'
+    molecule.write(join(mole_dir, mol_file))
 def main():
     if (not len(sys.argv) == 1):
         print("""
@@ -41,43 +89,16 @@ def main():
 
     # build ABCBA molecule
     pop_ids = (0, 0, 0)  # core, ligand, linker
-    large_part = build_ABCBA(core=core_pop[pop_ids[0]],
-                             liga=liga_pop[pop_ids[1]],
-                             link=link_pop[pop_ids[2]])
-    # output as built
-    prefix = core_pop[pop_ids[0]].name + '_'
-    prefix += liga_pop[pop_ids[0]].name + '_'
-    prefix += link_pop[pop_ids[0]].name
-    json_file = prefix + '_ABCBA.json'
-    large_part.dump(join(mole_dir, json_file))
-    mol_file = prefix + '_ABCBA.mol'
-    large_part.write(join(mole_dir, mol_file))
-    # energy minimize molecule
-    rdkit_ETKDG(large_part)
-    # output energy minimized
-    json_file = prefix + '_ABCBA_opt.json'
-    large_part.dump(join(mole_dir, json_file))
-    mol_file = prefix + '_ABCBA_opt.mol'
-    large_part.write(join(mole_dir, mol_file))
-
+    ABCBA_confs, ABCBA_molecule = get_molecule(type='ABCBA',
+                                               popns=(core_pop, liga_pop, link_pop),
+                                               pop_ids=pop_ids, N=5,
+                                               mole_dir=mole_dir)
     # build ABA molecule
     pop_ids = (0, 0, 0)  # core, ligand, linker
-    small_part = build_ABA(core=core_pop[0],
-                           liga=liga_pop[0])
-    # output as built
-    prefix = core_pop[pop_ids[0]].name + '_'
-    prefix += liga_pop[pop_ids[0]].name
-    json_file = prefix + '_ABA.json'
-    small_part.dump(join(mole_dir, json_file))
-    mol_file = prefix + '_ABA.mol'
-    small_part.write(join(mole_dir, mol_file))
-    # energy minimize molecule
-    rdkit_ETKDG(small_part)
-    # output energy minimized
-    json_file = prefix + '_ABA_opt.json'
-    small_part.dump(join(mole_dir, json_file))
-    mol_file = prefix + '_ABA_opt.mol'
-    small_part.write(join(mole_dir, mol_file))
+    ABA_confs, ABA_molecule = get_molecule(type='ABA',
+                                           popns=(core_pop, liga_pop, link_pop),
+                                           pop_ids=pop_ids, N=5,
+                                           mole_dir=mole_dir)
 
 
 if __name__ == "__main__":
