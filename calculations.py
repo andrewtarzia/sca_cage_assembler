c6e63d9ee121c300b616b5dc01a0977b0f02452a
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-04-08 17:29:43 +0100

    refactor general math functions

--- calculations.py
+++ calculations.py
@@ -10,6 +10,7 @@ Author: Andrew Tarzia
 Date Created: 15 Mar 2019
 """
 
+import numpy as np
 import MDAnalysis as mda
 from MDAnalysis.analysis.rms import rmsd
 import sys
@@ -22,7 +23,7 @@ def calculate_RMSD(results_part, structure_name, init_structure_dir):
 
     '''
     # read in initial structure
-    initial_structure_file = init_structure_dir+structure_name+'.xyz'
+    initial_structure_file = init_structure_dir + structure_name + '.xyz'
     ref = mda.Universe(initial_structure_file)
     # read in new structure
     new_structure_file = 'xtbopt.xyz'
@@ -56,3 +57,63 @@ def get_formation_energies(data, ff='OPLS'):
         FE = (row.cage_ey + stoich[row.topo]['h2o'] * H2O_energy) - (row.bb1_ey * stoich[row.topo]['bb1'] + row.bb2_ey * stoich[row.topo]['bb2'])
         form_energies.append(FE)
     return form_energies
+
+
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.
+
+    https://stackoverflow.com/questions/2827393/angles-between-two-n-dimensional-vectors-in-python/13849249#13849249
+    """
+    return vector / np.linalg.norm(vector)
+
+
+def angle_between(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+
+            >>> angle_between((1, 0, 0), (0, 1, 0))
+            1.5707963267948966
+            >>> angle_between((1, 0, 0), (1, 0, 0))
+            0.0
+            >>> angle_between((1, 0, 0), (-1, 0, 0))
+            3.141592653589793
+
+    https://stackoverflow.com/questions/2827393/angles-between-two-n-dimensional-vectors-in-python/13849249#13849249
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+
+def get_dihedral(pt1, pt2, pt3, pt4):
+    '''Calculate the dihedral (-pi to pi) between four points using Praxeolitic formula
+    1 sqrt, 1 cross product
+
+    From: https://stackoverflow.com/questions/20305272/dihedral-torsion-angle-from-four-points-in-cartesian-coordinates-in-python
+    (new_dihedral(p))
+    '''
+    p0 = pt1
+    p1 = pt2
+    p2 = pt3
+    p3 = pt4
+
+    b0 = -1.0 * (p1 - p0)
+    b1 = p2 - p1
+    b2 = p3 - p2
+
+    # normalize b1 so that it does not influence magnitude of vector
+    # rejections that come next
+    b1 /= np.linalg.norm(b1)
+
+    # vector rejections
+    # v = projection of b0 onto plane perpendicular to b1
+    #   = b0 minus component that aligns with b1
+    # w = projection of b2 onto plane perpendicular to b1
+    #   = b2 minus component that aligns with b1
+    v = b0 - np.dot(b0, b1) * b1
+    w = b2 - np.dot(b2, b1) * b1
+
+    # angle between v and w in a plane is the torsion angle
+    # v and w may not be normalized but that's fine since tan is y/x
+    x = np.dot(v, w)
+    y = np.dot(np.cross(b1, v), w)
+    return np.degrees(np.arctan2(y, x))
--- ligand_combiner/molecule_building.py
+++ ligand_combiner/molecule_building.py
@@ -19,66 +19,11 @@ from os.path import join
 from stk import rdkit_ETKDG
 sys.path.insert(0, '/home/atarzia/thesource/')
 from stk_functions import build_population, build_ABCBA, build_ABA
+from calculations import get_dihedral, angle_between
 
 
-def unit_vector(vector):
-    """ Returns the unit vector of the vector.
 
-    https://stackoverflow.com/questions/2827393/angles-between-two-n-dimensional-vectors-in-python/13849249#13849249
-    """
-    return vector / np.linalg.norm(vector)
-
-
-def angle_between(v1, v2):
-    """ Returns the angle in radians between vectors 'v1' and 'v2'::
-
-            >>> angle_between((1, 0, 0), (0, 1, 0))
-            1.5707963267948966
-            >>> angle_between((1, 0, 0), (1, 0, 0))
-            0.0
-            >>> angle_between((1, 0, 0), (-1, 0, 0))
-            3.141592653589793
-
-    https://stackoverflow.com/questions/2827393/angles-between-two-n-dimensional-vectors-in-python/13849249#13849249
-    """
-    v1_u = unit_vector(v1)
-    v2_u = unit_vector(v2)
-    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
-
-
-def get_dihedral(pt1, pt2, pt3, pt4):
-    '''Calculate the dihedral (-pi to pi) between four points using Praxeolitic formula
-    1 sqrt, 1 cross product
-
-    From: https://stackoverflow.com/questions/20305272/dihedral-torsion-angle-from-four-points-in-cartesian-coordinates-in-python
-    (new_dihedral(p))
     '''
-    p0 = pt1
-    p1 = pt2
-    p2 = pt3
-    p3 = pt4
-
-    b0 = -1.0 * (p1 - p0)
-    b1 = p2 - p1
-    b2 = p3 - p2
-
-    # normalize b1 so that it does not influence magnitude of vector
-    # rejections that come next
-    b1 /= np.linalg.norm(b1)
-
-    # vector rejections
-    # v = projection of b0 onto plane perpendicular to b1
-    #   = b0 minus component that aligns with b1
-    # w = projection of b2 onto plane perpendicular to b1
-    #   = b2 minus component that aligns with b1
-    v = b0 - np.dot(b0, b1) * b1
-    w = b2 - np.dot(b2, b1) * b1
-
-    # angle between v and w in a plane is the torsion angle
-    # v and w may not be normalized but that's fine since tan is y/x
-    x = np.dot(v, w)
-    y = np.dot(np.cross(b1, v), w)
-    return np.degrees(np.arctan2(y, x))
 
 
 def get_binding_N_coord(molecule, conf, frag_id):
