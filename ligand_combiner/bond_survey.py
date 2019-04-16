523d7aab4c1f26fceb0e2f6194ac277923b7bb1a
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-04-16 11:52:07 +0100

    added xtitle

--- ligand_combiner/bond_survey.py
+++ ligand_combiner/bond_survey.py
@@ -31,7 +31,8 @@ def main():
 
     histogram_plot_1(Y=data.DIST1, X_range=(1.8, 2.4),
                      width=0.05, alpha=1.0, color='purple',
-                     edgecolor='k', outfile='N_Pd_bond_survey.pdf', xtitle=None)
+                     edgecolor='k', outfile='N_Pd_bond_survey.pdf',
+                     xtitle='N-Pd bond distance [$\mathrm{\AA}$]')
 
     print('Mean = {} Angstrom'.format(data.DIST1.mean()))
     print('Standard Deviation = {} Angstrom'.format(data.DIST1.std()))
