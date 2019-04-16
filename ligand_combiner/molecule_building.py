0853f19e2d11aca4d7d4199533096e121d556889
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-04-16 14:46:10 +0100

    add option to avoid rebuild of all molecules

--- ligand_combiner/molecule_building.py
+++ ligand_combiner/molecule_building.py
@@ -321,9 +321,10 @@ def get_molecule(type, popns, pop_ids, inverted, N=1, mole_dir='./'):
 
 
 def main():
-    if (not len(sys.argv) == 2):
+    if (not len(sys.argv) == 6):
         print("""
-    Usage: molecule_builing.py N
+    Usage: molecule_builing.py rebuild N angle_tol bond_mean bond_std
+        rebuild (str) - 't' if you want to rebuild all molecules, 'f' to load populations
         N (int) - number of conformers to use
         angle_tol (float) - tolerance to use on angle matching
         bond_mean (float) - mean value of bond distance to use in candidate selection
@@ -331,6 +332,7 @@ def main():
         """)
         sys.exit()
     else:
+        rebuild = sys.argv[1]
         N = int(sys.argv[2])
         angle_tol = float(sys.argv[3])
         bond_mean = float(sys.argv[4])
@@ -342,78 +344,103 @@ def main():
     link_dir = proj_dir + 'linkers/'
     mole_dir = proj_dir + 'molecules/'
 
-    # build molecule populations
-    core_pop = build_population(directory=core_dir, structunit='StructUnit2',
-                                fgs=['bromine'])
-    liga_pop = build_population(directory=liga_dir, structunit='StructUnit',
-                                fgs=['bromine'])
-    link_pop = build_population(directory=link_dir, structunit='StructUnit2',
-                                fgs=['bromine'])
+    if rebuild == 't':
+        # build molecule populations
+        core_pop = build_population(directory=core_dir, structunit='StructUnit2',
+                                    fgs=['bromine'])
+        liga_pop = build_population(directory=liga_dir, structunit='StructUnit',
+                                    fgs=['bromine'])
+        link_pop = build_population(directory=link_dir, structunit='StructUnit2',
+                                    fgs=['bromine'])
 
-    # for the linker molecules, we attach an invertable flag attribute
-    link_mol_prop = pd.read_csv(join(link_dir, 'linkers.csv'))
-    for i in link_pop:
-        inversion_flag = str(link_mol_prop[link_mol_prop.name == i.name]['inversion_flag'].iloc[0])
-        if inversion_flag == 't':
-            i.invertable = True
-        else:
-            i.invertable = False
+        # for the linker molecules, we attach an invertable flag attribute
+        link_mol_prop = pd.read_csv(join(link_dir, 'linkers.csv'))
+        for i in link_pop:
+            inversion_flag = str(link_mol_prop[link_mol_prop.name == i.name]['inversion_flag'].iloc[0])
+            if inversion_flag == 't':
+                i.invertable = True
+            else:
+                i.invertable = False
 
-    # this is the resultant molecule population
-    molecule_pop = Population()
-    for i, core in enumerate(core_pop):
-        for j, liga in enumerate(liga_pop):
-            for k, link in enumerate(link_pop):
-                # build ABCBA molecule
-                pop_ids = (i, j, k)  # core, ligand, linker
-                print(pop_ids)
-                ABCBA_confs, ABCBA_molecule = get_molecule(
-                    type='ABCBA', inverted=False,
-                    popns=(core_pop, liga_pop, link_pop),
-                    pop_ids=pop_ids, N=N,
-                    mole_dir=mole_dir)
-                ABCBA_molecule.name = 'ABCBA_' + str(i) + str(j) + str(k)
-                # get properties - save to molecule as attribute
-                ABCBA_molecule = get_geometrical_properties(mol=ABCBA_molecule,
-                                                            cids=ABCBA_confs,
-                                                            type='ABCBA')
-                molecule_pop.members.append(ABCBA_molecule)
-                # also build the inverted molecule if possible.
-                if link_pop[pop_ids[2]].invertable is True:
-                    ABCBA_inv_confs, ABCBA_inv_molecule = get_molecule(
-                        type='ABCBA', inverted=True,
+        # this is the resultant molecule population
+        molecule_pop = Population()
+        for i, core in enumerate(core_pop):
+            for j, liga in enumerate(liga_pop):
+                for k, link in enumerate(link_pop):
+                    # build ABCBA molecule
+                    pop_ids = (i, j, k)  # core, ligand, linker
+                    print(pop_ids)
+                    ABCBA_confs, ABCBA_molecule = get_molecule(
+                        type='ABCBA', inverted=False,
                         popns=(core_pop, liga_pop, link_pop),
                         pop_ids=pop_ids, N=N,
                         mole_dir=mole_dir)
-                    ABCBA_inv_molecule.name = 'ABCBA_' + str(i) + str(j) + str(k) + 'i'
+                    ABCBA_molecule.name = 'ABCBA_' + str(i) + str(j) + str(k)
                     # get properties - save to molecule as attribute
-                    ABCBA_inv_molecule = get_geometrical_properties(mol=ABCBA_inv_molecule,
-                                                                    cids=ABCBA_inv_confs,
-                                                                    type='ABCBA')
-                    molecule_pop.members.append(ABCBA_inv_molecule)
-                break
-            # build ABA molecule
-            ABA_confs, ABA_molecule = get_molecule(
-                type='ABA', inverted=False,
-                popns=(core_pop, liga_pop, link_pop),
-                pop_ids=pop_ids, N=N,
-                mole_dir=mole_dir)
-            ABA_molecule.name = 'ABA_' + str(i) + str(j)
-            # get properties - save to molecule as attribute
-            ABA_molecule = get_geometrical_properties(mol=ABA_molecule,
-                                                      cids=ABA_confs,
-                                                      type='ABA')
-            molecule_pop.members.append(ABA_molecule)
+                    ABCBA_molecule = get_geometrical_properties(mol=ABCBA_molecule,
+                                                                cids=ABCBA_confs,
+                                                                type='ABCBA')
+                    molecule_pop.members.append(ABCBA_molecule)
+                    # also build the inverted molecule if possible.
+                    if link_pop[pop_ids[2]].invertable is True:
+                        ABCBA_inv_confs, ABCBA_inv_molecule = get_molecule(
+                            type='ABCBA', inverted=True,
+                            popns=(core_pop, liga_pop, link_pop),
+                            pop_ids=pop_ids, N=N,
+                            mole_dir=mole_dir)
+                        ABCBA_inv_molecule.name = 'ABCBA_' + str(i) + str(j) + str(k) + 'i'
+                        # get properties - save to molecule as attribute
+                        ABCBA_inv_molecule = get_geometrical_properties(mol=ABCBA_inv_molecule,
+                                                                        cids=ABCBA_inv_confs,
+                                                                        type='ABCBA')
+                        molecule_pop.members.append(ABCBA_inv_molecule)
+                    # break
+                # build ABA molecule
+                ABA_confs, ABA_molecule = get_molecule(
+                    type='ABA', inverted=False,
+                    popns=(core_pop, liga_pop, link_pop),
+                    pop_ids=pop_ids, N=N,
+                    mole_dir=mole_dir)
+                ABA_molecule.name = 'ABA_' + str(i) + str(j)
+                # get properties - save to molecule as attribute
+                ABA_molecule = get_geometrical_properties(mol=ABA_molecule,
+                                                          cids=ABA_confs,
+                                                          type='ABA')
+                molecule_pop.members.append(ABA_molecule)
+                # break
             # break
-        break
 
-    # draw 2D representation for all built molecules
-    mol_list = []
-    for poly in molecule_pop:
-        MOL = Chem.MolFromSmiles(Chem.MolToSmiles(poly.mol))
-        mol_list.append(MOL)
-    mol_list2grid(molecules=mol_list, filename='built_molecules',
-                  mol_per_row=3, maxrows=3, subImgSize=(200, 200))
+        # save populations
+        core_pop.dump(join(core_dir, 'core.pop'))
+        liga_pop.dump(join(liga_dir, 'ligands.pop'))
+        link_pop.dump(join(link_dir, 'linkers.pop'))
+        molecule_pop.dump(join(mole_dir, 'molecules.pop'))
+
+        # draw 2D representation for all built molecules
+        mol_list = []
+        for poly in molecule_pop:
+            MOL = Chem.MolFromSmiles(Chem.MolToSmiles(poly.mol))
+            mol_list.append(MOL)
+        mol_list2grid(molecules=mol_list, filename='built_molecules',
+                      mol_per_row=3, maxrows=3, subImgSize=(200, 200))
+
+    elif rebuild == 'f':
+        # load in populations
+        core_pop = stk.Population()
+        liga_pop = stk.Population()
+        link_pop = stk.Population()
+        molecule_pop = stk.Population()
+        core_pop = core_pop.load(path=join(core_dir, 'core.pop'),
+                                 member_init=stk.Molecule.from_dict)
+        liga_pop = liga_pop.load(path=join(liga_dir, 'ligands.pop'),
+                                 member_init=stk.Molecule.from_dict)
+        link_pop = link_pop.load(path=join(link_dir, 'linkers.pop'),
+                                 member_init=stk.Molecule.from_dict)
+        molecule_pop = molecule_pop.load(path=join(mole_dir, 'molecules.pop'),
+                                         member_init=stk.Molecule.from_dict)
+
+    print(molecule_pop)
+
     # define bond length vector to use based on N-Pd bond distances extracted
     # from survey
     # N-Pd-N length
