a18177c380867e8f1e37d7dcf114e002d1965ba9
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-04-23 15:35:19 +0100

    start working on plotting of all pair data for information purposes. no screening at this stage

--- ligand_combiner/analyze_molecules.py
+++ ligand_combiner/analyze_molecules.py
@@ -13,30 +13,61 @@ Date Created: 17 Apr 2019
 
 import sys
 import pickle
-import numpy as np
-import pandas as pd
-from rdkit.Chem import AllChem as Chem
 from os.path import join
 import matplotlib.pyplot as plt
-import stk
-from stk import Population
-from Combiner import get_molecule, get_geometrical_properties, Combination
-sys.path.insert(0, '/home/atarzia/thesource/')
-from stk_functions import build_population
-from rdkit_functions import mol_list2grid
+
+
+def plot_all_pair_info(pair_data, angle_tol, energy_tol, outfile):
+    '''Do multi dimensional plot of all molecule pair information.
+
+    '''
+    markers = ['o', 'X', 'P', 'D', '>', '<', ]
+    fig, ax = plt.subplots(figsize=(8, 5))
+
+    # define colour map based on energy tol
+
+    X_max = 0
+    Y_max = 0
+    for pair in pair_data:
+        if pair.test_N_N_lengths is False:
+            continue
+        # print(pair.__dict__)
+        pair.get_angle_deviations()
+        pair.get_N_Pd_lengths_deviation()
+        X = max(pair.angle1_deviation, pair.angle2_deviation)
+        Y = pair.NPdN_difference
+        Z = max([pair.energy1, pair.energy2])
+        X_max = max([X_max, X])
+        Y_max = max([Y_max, Y])
+        ax.scatter(X, Y, c='firebrick', edgecolors='k',
+                   marker=markers[pair.popn_ids[0]], alpha=1.0, s=80)
+        # break
+    ax.axhline(pair.tol, c='k', alpha=0.5)
+    ax.axvline(angle_tol, c='k', alpha=0.5)
+    # Set number of ticks for x-axis
+    ax.tick_params(axis='both', which='major', labelsize=16)
+    ax.set_xlabel('maximum angle deviation [degrees]', fontsize=16)
+    ax.set_ylabel('deviation from planarity', fontsize=16)
+    ax.set_xlim(0, round(X_max+1))
+    ax.set_ylim(0, round(Y_max+1))
+    fig.tight_layout()
+    fig.savefig(outfile, dpi=720,
+                bbox_inches='tight')
 
 
 def main():
-    if (not len(sys.argv) == 3):
+    if (not len(sys.argv) == 4):
         print("""
-    Usage: analyze_molecules.py pair_data angle_tol
+    Usage: analyze_molecules.py pair_data angle_tol energy_tol
         pair_data (str) - pickle file with pair data
         angle_tol (float) - tolerance to use on angle matching
+        energy_tol (float) - max kJ/mol over min energy conformer to allow
         """)
         sys.exit()
     else:
         pair_data = sys.argv[1]
-        angle_tol = sys.argv[2]
+        angle_tol = float(sys.argv[2])
+        energy_tol = float(sys.argv[3])
 
     proj_dir = '/home/atarzia/projects/ligand_combiner/'
     core_dir = proj_dir + 'cores/'
@@ -48,160 +79,10 @@ def main():
     with open(join(mole_dir, pair_data), 'rb') as f:
         all_pairs = pickle.load(f)
     print(len(all_pairs))
-                #
-                #
-                #
-                # NN_dist2s = []
-                # angles1_sums = []
-                # angles2_sums = []
-                # LHSs = []
-                # RHSs = []
-                #
-                #
-                #     print(poly1.name, conf1, poly2.name, conf2)
-                #     # obtain all properties
-                #     # check N-N distance of poly1-conf > poly2-conf
-                #     NN_dist1 = np.linalg.norm(PROP1['NN_v'])
-                #     NN_dist2 = np.linalg.norm(PROP2['NN_v'])
-                #     # determine angles made by NN_v and NN-BC_v
-                #     # check that the pairs sum to 180
-                #     p1_angle1 = PROP1['NN_BCN_1']
-                #     p1_angle2 = PROP1['NN_BCN_2']
-                #     p2_angle1 = PROP2['NN_BCN_1']
-                #     p2_angle2 = PROP2['NN_BCN_2']
-                #     # now check that the length of the long
-                #     # vector and the short vector are commensurate
-                #     # with an ideal trapezoid with the given angles
-                #     # i.e. the extender vector determined by the
-                #     # difference of the two NN_v (LHS) matches what is
-                #     # expected by trig (RHS)
-                #     extender_V_LHS = (NN_dist1 - NN_dist2) / 2
-                #     test_angle = np.radians(180 - p2_angle1)
-                #     extender_V_RHS = vector_length * np.cos(test_angle)
-                #     tol = 2 * vector_std * np.cos(test_angle)
-                #     # temporary for plotting
-                #     NN_dist2s.append(NN_dist2)
-                #     angles1_sums.append(p1_angle1 + p2_angle1)
-                #     angles2_sums.append(p1_angle2 + p2_angle2)
-                #     LHSs.append(extender_V_LHS)
-                #     RHSs.append(extender_V_RHS)
-                #     # run checks
-                #     if NN_dist1 > NN_dist2:
-                #         if np.isclose(p1_angle1 + p2_angle1, 180, rtol=0, atol=angle_tol):
-                #             if np.isclose(p1_angle2 + p2_angle2, 180, rtol=0, atol=angle_tol):
-                #                 if np.isclose(extender_V_LHS, extender_V_RHS,
-                #                               rtol=0, atol=tol):
-                #                     print(poly1.name, conf1, poly2.name, conf2)
-                #                     print('NN_dists:', NN_dist1, NN_dist2)
-                #                     print('angles:', p1_angle1,
-                #                           p1_angle2, p2_angle1, p2_angle2)
-                #                     print('sum angles:',
-                #                           p1_angle1 + p2_angle1,
-                #                           p1_angle2 + p2_angle2)
-                #                     print('LHS:', extender_V_LHS)
-                #                     print('cos(pi-d):', test_angle)
-                #                     print('RHS:', extender_V_RHS)
-                #                     print('bond_tol:', tol)
-                #                     print('passed')
-                #                     input()
-                #                     passed_pairs.append((i, j, conf1, conf2,
-                #                                          p1_angle1, p1_angle2,
-                #                                          p2_angle1, p2_angle2,
-                #                                          NN_dist1, NN_dist2))
-                #         all_pairs.append((i, j, conf1, conf2,
-                #                           p1_angle1, p1_angle2,
-                #                           p2_angle1, p2_angle2,
-                #                           NN_dist1, NN_dist2))
-                #     # make some temp plots
-                #     fig, ax = plt.subplots(figsize=(8, 5))
-                #     X_range = (5, 20)
-                #     width = 0.5
-                #     density = False
-                #     Y = NN_dist2s
-                #     xtitle = 'NN_dist2'
-                #     ax.axvline(x=NN_dist1, c='k')
-                #     X_bins = np.arange(X_range[0], X_range[1], width)
-                #     hist, bin_edges = np.histogram(a=Y, bins=X_bins,
-                #                                    density=density)
-                #     ax.bar(bin_edges[:-1],
-                #            hist,
-                #            align='edge',
-                #            alpha=1.0, width=width,
-                #            color='firebrick',
-                #            edgecolor='k')
-                #     ax.tick_params(axis='both', which='major', labelsize=16)
-                #     ax.set_xlabel(xtitle, fontsize=16)
-                #     if density is False:
-                #         ax.set_ylabel('count', fontsize=16)
-                #     elif density is True:
-                #         ax.set_ylabel('frequency', fontsize=16)
-                #     ax.set_xlim(X_range)
-                #     ax.set_title(poly1.name + '_' + str(conf1))
-                #     fig.tight_layout()
-                #     fig.savefig('NN_dists_' + poly1.name + '_' + str(conf1) + '_'+ poly2.name + '.pdf', dpi=720,
-                #                 bbox_inches='tight')
-                #     plt.close()
-                #
-                #     fig, ax = plt.subplots(figsize=(8, 5))
-                #     X_range = (90, 270)
-                #     width = 5
-                #     density = False
-                #     xtitle = 'sum of angles [degrees]'
-                #     ax.axvline(x=180 - angle_tol, c='k')
-                #     ax.axvline(x=180 + angle_tol, c='k')
-                #
-                #     Y = angles1_sums
-                #     X_bins = np.arange(X_range[0], X_range[1], width)
-                #     hist, bin_edges = np.histogram(a=Y, bins=X_bins,
-                #                                    density=density)
-                #     ax.bar(bin_edges[:-1],
-                #            hist,
-                #            align='edge',
-                #            alpha=0.6, width=width,
-                #            color='r',
-                #            edgecolor='k', label='angle1 sum')
-                #     Y = angles2_sums
-                #     X_bins = np.arange(X_range[0], X_range[1], width)
-                #     hist, bin_edges = np.histogram(a=Y, bins=X_bins,
-                #                                    density=density)
-                #     ax.bar(bin_edges[:-1],
-                #            hist,
-                #            align='edge',
-                #            alpha=0.6, width=width,
-                #            color='b',
-                #            edgecolor='k', label='angle2 sum')
-                #
-                #     ax.tick_params(axis='both', which='major', labelsize=16)
-                #     ax.set_xlabel(xtitle, fontsize=16)
-                #     if density is False:
-                #         ax.set_ylabel('count', fontsize=16)
-                #     elif density is True:
-                #         ax.set_ylabel('frequency', fontsize=16)
-                #     ax.set_xlim(X_range)
-                #     ax.set_title(poly1.name + '_' + str(conf1))
-                #     ax.legend()
-                #     fig.tight_layout()
-                #     fig.savefig('angles_dists_' + poly1.name + '_' + str(conf1) + '_'+ poly2.name + '.pdf', dpi=720,
-                #                 bbox_inches='tight')
-                #     plt.close()
-                #
-                #     fig, ax = plt.subplots(figsize=(5, 5))
-                #     X_range = (0, 5)
-                #     X = LHSs
-                #     Y = RHSs
-                #     ax.scatter(X, Y, alpha=0.6, color='r', edgecolor='k')
-                #     ax.plot(np.linspace(0, X_range[1], 10),
-                #             np.linspace(0, X_range[1], 10), alpha=0.2, c='k')
-                #     ax.tick_params(axis='both', which='major', labelsize=16)
-                #     ax.set_xlabel('LHS', fontsize=16)
-                #     ax.set_ylabel('RHS', fontsize=16)
-                #     ax.set_xlim(X_range)
-                #     ax.set_ylim(X_range)
-                #     ax.set_title(poly1.name + '_' + str(conf1))
-                #     fig.tight_layout()
-                #     fig.savefig('LHSRHS_parity_' + poly1.name + '_' + str(conf1) + '_'+ poly2.name + '.pdf', dpi=720,
-                #                 bbox_inches='tight')
-                #     plt.close()
+
+    plot_all_pair_info(pair_data=all_pairs,
+                       angle_tol=angle_tol, energy_tol=energy_tol,
+                       outfile=pair_data.replace('.pkl', '.pdf'))
 
 
 if __name__ == "__main__":
