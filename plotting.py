7a9d71b49205b5a185cec9a0045e30fe339441d1
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-04-23 20:30:15 +0100

    added cmap to plotting functions

--- plotting.py
+++ plotting.py
@@ -12,6 +12,7 @@ Date Created: 25 Mar 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 
 def parity_plot(X, Y, outfile, xtitle, ytitle, lim):
@@ -65,3 +66,66 @@ def histogram_plot_1(Y, X_range, width, alpha, color, edgecolor,
 def flat_line(ax, x, y, w=0, C='k', m='x'):
     ax.plot([x - w, x, x + w], [y, y, y], c=C)
     ax.scatter(x, y, marker=m, c=C)
+
+
+def shiftedColorMap(cmap, start=0, midpoint=0.5, stop=1.0, name='shiftedcmap'):
+    '''
+    Function to offset the "center" of a colormap. Useful for
+    data with a negative min and positive max and you want the
+    middle of the colormap's dynamic range to be at zero
+
+    From Stack Exchange:
+        https://stackoverflow.com/questions/7404116/defining-the-midpoint-of-a-colormap-in-matplotlib
+
+    Input
+    -----
+      cmap : The matplotlib colormap to be altered
+      start : Offset from lowest point in the colormap's range.
+          Defaults to 0.0 (no lower ofset). Should be between
+          0.0 and `midpoint`.
+      midpoint : The new center of the colormap. Defaults to
+          0.5 (no shift). Should be between 0.0 and 1.0. In
+          general, this should be  1 - vmax/(vmax + abs(vmin))
+          For example if your data range from -15.0 to +5.0 and
+          you want the center of the colormap at 0.0, `midpoint`
+          should be set to  1 - 5/(5 + 15)) or 0.75
+      stop : Offset from highets point in the colormap's range.
+          Defaults to 1.0 (no upper ofset). Should be between
+          `midpoint` and 1.0.
+    '''
+    cdict = {
+        'red': [],
+        'green': [],
+        'blue': [],
+        'alpha': []
+    }
+    # regular index to compute the colors
+    reg_index = np.linspace(start, stop, 257)
+    # shifted index to match the data
+    shift_index = np.hstack([
+        np.linspace(0.0, midpoint, 128, endpoint=False),
+        np.linspace(midpoint, 1.0, 129, endpoint=True)
+    ])
+
+    for ri, si in zip(reg_index, shift_index):
+        r, g, b, a = cmap(ri)
+        cdict['red'].append((si, r, r))
+        cdict['green'].append((si, g, g))
+        cdict['blue'].append((si, b, b))
+        cdict['alpha'].append((si, a, a))
+    newcmap = colors.LinearSegmentedColormap(name, cdict)
+    plt.register_cmap(cmap=newcmap)
+    return newcmap
+
+
+def define_plot_cmap(fig, ax, mid_point, cmap, ticks, labels, cmap_label):
+    """Define cmap shifted to midpoint and plot colourbar
+
+    """
+    new_cmap = shiftedColorMap(cmap, midpoint=mid_point, name='shifted')
+    X = np.linspace(0, 1, 256)
+    cax = ax.scatter(-X-100, -X-100, c=X, cmap=new_cmap)
+    cbar = fig.colorbar(cax, ticks=ticks, spacing='proportional')
+    cbar.ax.set_yticklabels(labels, fontsize=16)
+    cbar.set_label(cmap_label, fontsize=16)
+    return new_cmap
