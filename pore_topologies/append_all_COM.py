0da7505f5c69610f72763f2e9e2c069d3fbfb7f0
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-04-24 18:07:38 +0100

    allow for batch run over multiple CIFs given a wildcard

--- pore_topologies/append_all_COM.py
+++ pore_topologies/append_all_COM.py
@@ -18,26 +18,36 @@ from IO_tools import convert_CIF_2_PDB
 
 
 if __name__ == "__main__":
-    if (not len(sys.argv) == 2):
+    if (not len(sys.argv) == 3):
         print("""
-Usage: append_all_COM.py CIF
-    CIF: file (.cif) to analyze and add pseudo atoms to.
+Usage: append_all_COM.py CIF ignore
+    CIF: file (.cif) to analyze and add pseudo atoms to ('*.cif' for all in working dir)
+    ignore (str) - string to use to ignore certain files (set NONE if not used)
     """)
         sys.exit()
+    if '*' in sys.argv[1]:
+        from glob import glob
+        if sys.argv[2] != 'NONE':
+            CIFs = sorted([i for i in glob(sys.argv[1]) if sys.argv[2] not in i])
+        else:
+            CIFs = sorted([i for i in glob(sys.argv[1])])
+        print('{} CIFs to analyze'.format(len(CIFs)))
     else:
-        file = sys.argv[1]
-    pdb_file, ASE_structure = convert_CIF_2_PDB(file)
-    if pdb_file is None and ASE_structure is None:
-        sys.exit()
-    # rebuild system
-    rebuilt_structure = rebuild_system(file=pdb_file)
-    rebuilt_structure.make_modular()
-    # run analysis
-    COM_dict = analyze_rebuilt(rebuilt_structure, atom_limit=20,
-                               file_prefix=file.replace('.cif', ''),
-                               verbose=False, include_coms=True)
-    # append atoms to ASE structure as pseudo atoms and write out new CIF
-    append_and_write_COMs(COM_dict, ASE_structure, file)
+        CIFs = [sys.argv[1]]
+
+    for file in CIFs:
         # do not redo
         if isfile(file.replace('.cif', '_appended.cif')):
             continue
+        pdb_file, ASE_structure = convert_CIF_2_PDB(file)
+        if pdb_file is None and ASE_structure is None:
+            continue
+        # rebuild system
+        rebuilt_structure = rebuild_system(file=pdb_file)
+        rebuilt_structure.make_modular()
+        # run analysis
+        COM_dict = analyze_rebuilt(rebuilt_structure, atom_limit=20,
+                                   file_prefix=file.replace('.cif', ''),
+                                   verbose=False, include_coms=True)
+        # append atoms to ASE structure as pseudo atoms and write out new CIF
+        append_and_write_COMs(COM_dict, ASE_structure, file)
