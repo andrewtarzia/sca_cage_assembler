2ba87339890a69237e3d8794aaf73c31bd21b330
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-05-03 23:25:55 +0100

    standardize file naming based on whether it is being run on cages or building blocks. debug GFN calculations

--- andrew_marsh_structures/get_all_struct_energies.py
+++ andrew_marsh_structures/get_all_struct_energies.py
@@ -18,6 +18,7 @@ import pandas as pd
 sys.path.insert(0, '/home/atarzia/thesource/')
 from GFN_functions import run_GFN_base, get_energies
 from stk_functions import optimize_structunit, atarzia_long_MD_settings
+from IO_tools import convert_MOL3000_2_PDB_XYZ
 
 
 def main():
@@ -47,36 +48,55 @@ Usage: get_all_struct_energies.py output_file suffix
 
     done_files = list(DATA['files'])
     # iterate over all files with suffix
-    files = glob('*' + suffix)
+    if suffix != '_opt.mol':
+        # not on cages
+        files = [i for i in glob('*' + suffix) if '_opt.mol' not in i]
+    else:
+        # being run on cages
+        files = [i for i in glob('*' + suffix)]
     # ignore done files
-    files = [i for i in files if i not in done_files]
+    files = [i for i in files
+             if i.replace('.mol', '_opt.mol') not in done_files]
+    print('files to do GFN:', files)
+    print('files done:', done_files)
     if MD.lower() == 't':
         # do a macromodelMD conformer search with stk first
         outfiles = []
         for file in files:
             OUTFILE = file.replace('.mol', '_opt.mol')
-            optimize_structunit(infile=file,
-                                outfile=OUTFILE,
-                                exec=macromod_,
-                                settings=atarzia_long_MD_settings())
+            if isfile(OUTFILE) is False:
+                optimize_structunit(infile=file,
+                                    outfile=OUTFILE,
+                                    exec=macromod_,
+                                    settings=atarzia_long_MD_settings())
             outfiles.append(OUTFILE)
     else:
-        outfiles = files
+        outfiles = [i.replace('.mol', '_opt.mol') for i in files
+                    if i.replace('.mol', '_opt.mol') not in done_files]
 
+    print('output files to analyze:', outfiles)
     # do GFN optimization
-    xyzs = [i.replace('.mol', '.xyz') for i in outfiles]
+    # no need to rerun structures already in DATA
+    xyzs = [i.replace('.mol', '.xyz') for i in outfiles
+            if i.rstrip('.mol') not in list(DATA['NAMES'])]
+    print('XYZs to do:', xyzs)
+    # convert outfiles (.mol) to XYZ files (.xyz)
+    for file in outfiles:
+        if file.rstrip('.xyz') not in list(DATA['NAMES']):
+            convert_MOL3000_2_PDB_XYZ(file=file)
+
     failed = run_GFN_base(xyzs=xyzs)
     if len(failed) > 0:
         sys.exit('---> Some GFN calcs failed. Exitting.')
 
     # get Free energies and save to output_file
-    for file in xyzs:
-        DIR = file.replace('.xyz', '')
-        OUT = file.replace('.xyz', '.out')
-        ORIG_FILE = file.replace('.xyz', '.mol')
-        energy_results = get_energies(output_file=join(DIR, OUT))
-        DATA = DATA.append({'NAMES': file.rstrip('.xyz'),
-                            'files': ORIG_FILE,
+    for file in outfiles:
+        DIR = file.replace('.mol', '')
+        OUT = file.replace('.mol', '.output')
+        energy_results = get_energies(output_file=join(DIR, OUT),
+                                      GFN_exec='/home/atarzia/software/xtb_190418/bin/xtb')
+        DATA = DATA.append({'NAMES': file.rstrip('.mol'),
+                            'files': file,
                             'FE (au)': energy_results['FE']},
                            ignore_index=True)
         # write dataFrame
