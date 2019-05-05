a370f6842e082d2e3d035a127a43dd3d77599b46
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-05-05 15:08:07 +0100

    finish (up to testing stage) of script to extract the formation energies of all cages in a directory from precalculated energies in files

--- andrew_marsh_structures/extract_cage_formEY.py
+++ andrew_marsh_structures/extract_cage_formEY.py
@@ -10,6 +10,24 @@ Author: Andrew Tarzia
 Date Created: 03 May 2019
 """
 
+import sys
+from os.path import isfile
+from glob import glob
+import pandas as pd
+sys.path.insert(0, '/home/atarzia/thesource/')
+from stk_functions import topo_2_property
+from plotting import histogram_plot_1
+
+
+def get_energy_from_DF(DF, name):
+    '''Return the float of the FE ('FE (au)')) from a pandas DataFrame.
+
+    '''
+    rowDF = DF[DF['NAMES'] == name].iloc[0]
+    value = rowDF['FE (au)']
+    return float(value)
+
+
 def main():
     """Run script.
 
@@ -17,14 +35,81 @@ def main():
     if (not len(sys.argv) == 4):
         print("""
 Usage: get_all_struct_energies.py output_file suffix
-    output_file: file to output results
-    suffix (str) - file suffix to run GFN calculation on - should end in .mol
-    MD (str) - 't' if a macromodel conformer search should be applied
+    output_file (str): file to output results
+    cage_energy (str): file with cage energies
+    prec_energy (str): file with energies of all required precursors
 """)
         sys.exit()
     else:
         output_file = sys.argv[1]
-        suffix = sys.argv[2]
-        MD = sys.argv[3]
+        cage_file = sys.argv[2]
+        prec_file = sys.argv[3]
+
+    if isfile(output_file):
+        # read in data
+        DATA = pd.read_csv(output_file)
+    else:
+        DATA = pd.DataFrame(columns=['file', 'cagename', 'bb1', 'bb2', 'topo', 'FE (au)'])
+
+    # read in energy files
+    cage_energies = pd.read_csv(cage_file)
+    prec_energies = pd.read_csv(prec_file)
+    print(cage_energies)
+    print('---')
+    print(prec_energies)
+
+    done_files = list(DATA['file'])
+    cage_opt_files = glob('*_opt.mol')
+
+    # remove the cages for which the formation energies have been output
+    cage_opt_files = [i for i in cage_opt_files if i not in done_files]
+    # get the formation energies of the remaining, while updating the output file
+    for file in cage_opt_files:
+        print(file)
+        # naming
+        name = file.replace('_opt.mol', '')
+        bb1 = name.split('_')[0]  # alde
+        bb2 = '_'.join(name.split('_')[1:3])  # amine
+        topo = name.split('_')[3]
+        # get cage energy
+        cage_energy = get_energy_from_DF(DF=prec_energies, name=name)
+        print('c', cage_energy)
+        # get water energy * noimines formed
+        noimines = topo_2_property(topology=topo, property='noimines')
+        water_energy = get_energy_from_DF(DF=prec_energies, name='water') * noimines
+        print('w', water_energy, water_energy / noimines)
+        # get bb1 energy * stoich
+        bb1_stoich, bb2_stoich = topo_2_property(topology=topo, property='stoich')
+        bb1_energy = get_energy_from_DF(DF=prec_energies, name=bb1) * bb1_stoich
+        print('bb1', bb1_energy, bb1_energy / bb1_stoich, bb1_stoich)
+        # get bb2 energy * stoich
+        bb2_energy = get_energy_from_DF(DF=prec_energies, name=bb2) * bb2_stoich
+        print('bb2', bb2_energy, bb2_energy / bb2_stoich, bb2_stoich)
+        # calc formation energy
+        form_energy = (cage_energy + water_energy) - (bb1_energy + bb2_energy)
+        print(form_energy)
+        DATA = DATA.append({'file': file,
+                            'cagename': name,
+                            'bb1': bb1,
+                            'bb2': bb2,
+                            'topo': topo,
+                            'FE (au)': form_energy},
+                           ignore_index=True)
+        print(DATA)
+        sys.exit()
+        # write dataFrame
+        DATA.to_csv(output_file, index=False)
+
+    # plot histogram of formation energies
+    histogram_plot_1(Y=DATA['FE (au)'],
+                     X_range=(-1000, 1000),
+                     width=10,
+                     alpha=1,
+                     color='mediumvioletred',
+                     edgecolor='k',
+                     outfile='cage_formEY_GFN.pdf',
+                     xtitle='formation energy [a.u.]')
+
+
 if __name__ == "__main__":
     main()
