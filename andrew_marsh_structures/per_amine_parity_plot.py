b4df172c0dde138552ff878cf84dacd1ff87ab2a
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-05-05 14:11:02 +0100

    add comparison of OPLS Formation energies (still has debugging statements for when this is used properly)

--- andrew_marsh_structures/per_amine_parity_plot.py
+++ andrew_marsh_structures/per_amine_parity_plot.py
@@ -18,7 +18,112 @@ import matplotlib.pyplot as plt
 from os.path import isfile
 from stk import Cage, StructUnit2, StructUnit3
 sys.path.insert(0, '/home/atarzia/thesource/')
-from stk_functions import topo_2_property
+from stk_functions import topo_2_property, get_OPLS3_energy_of_list, atarzia_short_MD_settings
+from calculations import calc_formation_energy
+
+
+def get_all_formEY(property_name, output_file, list_of_names):
+    '''Return Dataframe containing all formation energy values for all cages.
+
+    Formation energy defined as OPLS energies in this case.
+
+    '''
+    # files containing calculated energies:
+    precursor_dir = '/home/atarzia/projects/andrew_marsh_structures/precursor_lib/'
+    prec_ey_file = precursor_dir+'all_prec_ey.json'
+    cage_ey_file = 'all_cage_ey.json'
+    macromod_ = '/home/atarzia/software/schrodinger_install'
+
+    NEWCSV = output_file.replace('.csv', '_'+property_name+'.csv')
+    # check if an output file exists.
+    if isfile(NEWCSV):
+        data_Frame = pd.read_csv(NEWCSV)
+    else:
+        # need to calculate the bb_distortion of all cages
+        data_Frame = pd.DataFrame(columns=['NAME', 'value', 'topo', 'alde', 'amine'])
+    done_list = list(set(data_Frame['NAME']))
+    count = 1
+    for name in list_of_names:
+        if name in done_list:
+            count += 1
+            continue
+        print('{} of {}'.format(count, len(list_of_names)))
+        alde_name = name.split('_')[0]
+        amine_name = '_'.join(name.split('_')[1:3])
+        topo = name.split('_')[3]
+
+        alde_dir = '/home/atarzia/projects/andrew_marsh_structures/precursor_lib/'
+        alde_file = alde_dir+alde_name+'.mol'
+        alde_struc = StructUnit3(alde_file, ['aldehyde'])
+        amine_dir = '/data/atarzia/precursor_DBs/reaxys_sorted/'
+        if '2f' in amine_name:
+            amine_dir += 'amines2f/'
+            amine_file = amine_dir+amine_name+'.mol'
+            amine_struc = StructUnit2(amine_file, ['amine'])
+        elif '3f' in amine_name:
+            amine_dir += 'amines3f/'
+            amine_file = amine_dir+amine_name+'.mol'
+            amine_struc = StructUnit3(amine_file, ['amine'])
+
+        topology = topo_2_property(topo, 'stk_func')
+        cage = Cage([alde_struc, amine_struc], topology)
+        cage.update_from_mol(name+'_opt.mol')
+        # calculate precusor energy
+        # aldehyde
+        print(name)
+        print('-----')
+        alde_energy = get_OPLS3_energy_of_list(out_file=prec_ey_file,
+                                               structures=[alde_file],
+                                               dir=alde_dir,
+                                               macromod_=macromod_,
+                                               opt=True,
+                                               settings=atarzia_short_MD_settings())
+        alde_energy = alde_energy[alde_name]
+        print('alde_ E', alde_energy)
+        # amine
+        amine_energy = get_OPLS3_energy_of_list(out_file=prec_ey_file,
+                                                structures=[amine_file],
+                                                dir=amine_dir,
+                                                macromod_=macromod_,
+                                                opt=True,
+                                                settings=atarzia_short_MD_settings())
+        amine_energy = amine_energy[amine_name]
+        print('amine E', amine_energy)
+        # cage
+        cage_file = name
+        cage_dir = ''
+        cage_energy = get_OPLS3_energy_of_list(out_file=cage_ey_file,
+                                                structures=[cage_file],
+                                                dir=cage_dir,
+                                                macromod_=macromod_,
+                                                opt=True,
+                                                settings=atarzia_short_MD_settings())
+        cage_energy = cage_energy[name]
+        print('CE', cage_energy)
+        # calculate formation energy from list of product and reactant energies
+        # OPLS energy of water = 0 UNITS
+        products = [0, cage_energy]
+        print('PR', products)
+        stoich = topo_2_property(topology=topo, property='stoich')
+        print('stoich', stoich)
+        # in this case, we can hard code the stoichiometries
+        alde_stoich, amine_stoich = stoich
+        reactants = [alde_energy] * alde_stoich + [amine_energy] * amine_stoich
+        print('Re', reactants)
+        formation_energy = calc_formation_energy(products, reactants)
+        print('FE', formation_energy)
+        input()
+        data_Frame = data_Frame.append({'NAME': name,
+                                        'value': formation_energy,
+                                        'amine': amine_name,
+                                        'alde': alde_name,
+                                        'topo': topo},
+                                       ignore_index=True)
+        count += 1
+    # write dataFrame
+    data_Frame.to_csv(NEWCSV,
+                      index=False)
+    return data_Frame
 
 
 def main():
@@ -33,6 +138,7 @@ Usage: per_amine_parity_plot.py output_file property
         Defined:
             p_diam_opt - pore diameter in Angstrom
             bb_dist - distortion of building blocks
+            oplsFE - formation energy of cage from OPLS energy
 """)
         sys.exit()
     else:
@@ -59,7 +165,10 @@ Usage: per_amine_parity_plot.py output_file property
                                           round(max(full_dataset['p_diam_opt']))+5)},
                   'bb_dist': {'axis_title': 'mean RMSD [$\mathrm{\AA}$]',
                               'column': None,  # needs to be calculated
-                              'lims': (0, 6)}
+                              'lims': (0, 6)},
+                  'oplsFE': {'axis_title': 'formation energy [kJ/mol]',
+                             'column': None,  # needs to be calculated
+                             'lims': (-100, 100)}
                   }
     property = properties[property_name]
     if property_name == 'bb_dist':
