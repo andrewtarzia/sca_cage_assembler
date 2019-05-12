cdc4e3074da0b5730cd85c307b432af98caeaebb
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-05-12 17:28:48 +0100

    refactor script. add choice between organic and organometallic systems

--- cage_collect/CSD_API/get_from_author.py
+++ cage_collect/CSD_API/get_from_author.py
@@ -12,6 +12,7 @@ Date Created: 1 Mar 2019
 """
 from ccdc.io import EntryReader, CrystalWriter
 from ccdc.search import TextNumericSearch
+import sys
 
 
 def write_entry(author, number, DOI, CSD, solvent, disorder):
@@ -31,69 +32,95 @@ def write_REFCODES(CSD):
         f.write(CSD+'\n')
 
 
-author_file = 'author_list.txt'
-# files = []
-authors = []
-DOIs = []
-CSD = []
-for line in open(author_file, 'r'):
-    authors.append(line.rstrip())
+def main():
+    if (not len(sys.argv) == 3):
+        print """
+    Usage: get_from_author.py bond_mean bond_std energy_tol angle_tol mol1 mol2
+        author_file (str) - file with list of authors
+        cage_type (str) - organic if organic cages, metal if is_organometallic
+            organic: sets is_organometallic is False
+            metal: sets is_organometallic is True
+            anything else: passes this test
+        """
+        sys.exit()
+    else:
+        author_file = sys.argv[1]
+        cage_type = sys.argv[2]
 
-with open('CIF_DB_author.txt', 'w') as f:
-    f.write('author,number,DOI,CSD,solvent,disorder\n')
+    # files = []
+    authors = []
+    DOIs = []
+    CSD = []
+    for line in open(author_file, 'r'):
+        authors.append(line.rstrip())
 
-with open('DB.gcd', 'w') as f:
-    f.write('')
+    with open('CIF_DB_author.txt', 'w') as f:
+        f.write('author,number,DOI,CSD,solvent,disorder\n')
 
-count = 0
-count_no = 0
-idents = []
-for i, author in enumerate(authors):
-    # break at '-----'
-    if '-----' in author:
-        break
-    count_no += 1
-    query = TextNumericSearch()
-    query.add_author(author)
-    hits = query.search(database='CSD')
-    if len(hits) == 0:
-        print(author)
-    for hit in hits:
-        # print('%s %s' % (hit.identifier, hit.entry.ccdc_number))
-        # print(hit.entry.chemical_name)
-        # print(hit.entry.is_polymeric)
-        author_list = [i.strip() for i in hit.entry.publication.authors.split(',')]
-        # skip polymeric structures
-        if hit.entry.chemical_name is not None:
-            if 'catena' in hit.entry.chemical_name:
+    with open('DB.gcd', 'w') as f:
+        f.write('')
+
+    count = 0
+    count_no = 0
+    idents = []
+    for i, author in enumerate(authors):
+        # break at '-----'
+        if '-----' in author:
+            break
+        count_no += 1
+        query = TextNumericSearch()
+        query.add_author(author)
+        hits = query.search(database='CSD')
+        print author+': '+str(len(hits))
+        if len(hits) == 0:
+            print(author)
+        for hit in hits:
+            # print('%s %s' % (hit.identifier, hit.entry.ccdc_number))
+            # print(hit.entry.chemical_name)
+            # print(hit.entry.is_polymeric)
+            author_list = [i.strip() for i in hit.entry.publication.authors.split(',')]
+            # skip polymeric structures
+            if hit.entry.chemical_name is not None:
+                if 'catena' in hit.entry.chemical_name:
+                    continue
+            if hit.entry.is_polymeric is True:
                 continue
-        if hit.entry.is_polymeric is True:
-            continue
-        # skip if structure is powder study
-        if hit.entry.is_powder_study is True:
-            continue
-        # skip structures that are purely organic
-        if hit.entry.is_organometallic is False:
-            continue
-        # print('passed!')
-        # note structures with solvent
-        solvent = 'n'
-        if hit.entry.chemical_name is not None:
-            if len(hit.entry.chemical_name.split(' ')) > 1:
-                solvent = 'y'
-        # note structures with disorder
-        disorder = 'n'
-        if hit.entry.has_disorder is True:
-            disorder = 'y'
-        crystal = hit.crystal
-        # write to CIF
-        if hit.identifier not in idents:
-            idents.append(hit.identifier)
-            # CrystalWriter(hit.identifier+'.cif').write(crystal)
-            write_entry(author, str(hit.entry.ccdc_number),
-                        hit.entry.doi, hit.identifier, solvent,
-                        disorder)
-            write_REFCODES(hit.identifier)
-            count += 1
+            # skip if structure is powder study
+            if hit.entry.is_powder_study is True:
+                continue
+            if cage_type == 'organic':
+                # skip structures that are NOT purely organic
+                if hit.entry.is_organometallic is True:
+                    continue
+            elif cage_type == 'metal':
+                # skip structures that are purely organic
+                if hit.entry.is_organometallic is False:
+                    continue
+            else:
+                # do not skip any
+                pass
+            # note structures with solvent
+            solvent = 'n'
+            if hit.entry.chemical_name is not None:
+                if len(hit.entry.chemical_name.split(' ')) > 1:
+                    solvent = 'y'
+            # note structures with disorder
+            disorder = 'n'
+            if hit.entry.has_disorder is True:
+                disorder = 'y'
+            crystal = hit.crystal
+            # write REFCODE to file
+            if hit.identifier not in idents:
+                idents.append(hit.identifier)
+                # CrystalWriter(hit.identifier+'.cif').write(crystal)
+                write_entry(author, str(hit.entry.ccdc_number),
+                            hit.entry.doi, hit.identifier, solvent,
+                            disorder)
+                write_REFCODES(hit.identifier)
+                count += 1
 
-print(count, 'cifs found from', count_no, 'authors')
+    print str(count)+' cifs found from '+str(count_no)+' authors'
+
+
+if __name__ == "__main__":
+    main()
