cbb03df40c44d87a2ea09cf0de0fb6c6815d3ad2
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-05-12 22:44:09 +0100

    rewrite sort_structures.py for new POC_list methodology. Checks for porosity, deletes CIFs if no porous molecule exists. keeps information

--- cage_collect/sort_structures.py
+++ cage_collect/sort_structures.py
@@ -11,39 +11,81 @@ Date Created: 05 Apr 2019
 
 """
 
+import logging
 import sys
-from glob import glob
-from os.path import isfile
-import json
+import pandas as pd
+import glob
+import os
 sys.path.insert(0, '/home/atarzia/thesource/')
+import pywindow_f
+import IO_tools
 
 
 def main():
-    all_cifs = sorted(glob('*extracted.cif')) + sorted(glob('*extractedm.cif'))
+    if (not len(sys.argv) == 3):
+        print("""
+    Usage: sort_structures.py DB_file output_file
+        DB_file (str) - file with initial list of REFCODEs
+        output_file (str) - file to output results of sorting to
+        """)
+        sys.exit()
+    else:
+        DB_file = sys.argv[1]
+        output_file = sys.argv[2]
 
-    for cif in all_cifs:
-        cif_out_1 = cif.replace('.cif', '_cleaned1.cif')
-        cif_out_2 = cif.replace('.cif', '_cleaned_P1.cif')
-        RC = cif.split('_')[0]
-        print(RC)
-        RC_files = glob(RC + '_*')
-        print(RC_files)
-        # remove those that got skipped in structure_preparation.py
-        if isfile(cif_out_1) is False and isfile(cif_out_2) is False:
+
+    refcodes = sorted([i.rstrip() for i in open(DB_file, 'r').readlines()])
+    cifs = [i+'_extracted.cif' for i in refcodes]
+    logging.info(f'> started with: {len(refcodes)} CIFs to sort.')
+    if os.path.isfile(output_file):
+        # read CIFs already checked to avoid double calculations
+        OUTDATA = pd.read_csv(output_file)
+        done_cifs = list(OUTDATA.cif)
+        logging.info(f'> {len(done_cifs)} CIFs already done.')
+    else:
+        # write output file
+        with open(output_file, 'w') as f:
+            f.write('cif,deleted\n')
+        OUTDATA = pd.read_csv(output_file)
+        done_cifs = []
+
+    # iterate over CIFs
+    count = len(done_cifs)
+    for cif in cifs:
+        # skip done cifs
+        if cif in done_cifs:
             continue
-        # remove those without .json files
-        json_files = glob(RC + '*.json')
-        if not len(json_files):
-            continue
-        for js in json_files:
-            print(js)
-            with open(js, 'r') as f:
-                data = json.load(f)
-            print(data['pore_diameter_opt']['diameter'])
-            print(data['pore_volume_opt'])
-            print(data['windows']['diameters'])
-        break
+        if os.path.isfile(cif):
+            pdb = IO_tools.convert_CIF_2_PDB(cif, wstruct=False)
+            logging.info(f'> doing {count} of {len(cifs)}')
+            # check if at least one molecule has a pore_diameter_opt > 0.25 angstrom
+            if pywindow_f.check_PDB_for_pore(file=pdb, diam=0.25):
+                OUTDATA = OUTDATA.append({'cif': cif, 'deleted': 'N'},
+                                         ignore_index=True)
+            else:
+                # delete molecule if not
+                OUTDATA = OUTDATA.append({'cif': cif, 'deleted': 'Y'},
+                                         ignore_index=True)
+                os.remove(cif)
+                os.remove(pdb)
+                os.remove(pdb.replace('.pdb', '_rebuild.pdb'))
+        else:
+            # CIF missing.
+            OUTDATA = OUTDATA.append({'cif': cif, 'deleted': 'M'},
+                                     ignore_index=True)
+
+        # add to done cifs
+        done_cifs.append(cif)
+        # update output file
+        OUTDATA.to_csv(output_file, index=False)
+        count += 1
+
+    remaining = list(OUTDATA[OUTDATA['deleted'] == 'N'])
+    logging.info(f'> ended with: {len(remaining)} CIFs.')
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s-%(message)s')
+    logging.debug(f'Debug mode!')
+    # logging.basicConfig(level=logging.INFO, format='')
     main()
