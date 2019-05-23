f9ef342d12e9f82fe25f94bb15de33db33e9c1ff
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-05-23 17:10:39 +0100

    completed

--- cage_collect/extract_most_porous.py
+++ cage_collect/extract_most_porous.py
@@ -33,6 +33,80 @@ def main():
         DB_file = sys.argv[1]
         output_file = sys.argv[2]
 
+    refcodes = sorted([i.rstrip() for i in open(DB_file, 'r').readlines()])
+    pdbs = [i+'_extracted.pdb' for i in refcodes]
+    logging.info(f'> started with: {len(refcodes)} structures to classify.')
+    if os.path.isfile(output_file):
+        # read CIFs already checked to avoid double calculations
+        OUTDATA = pd.read_csv(output_file)
+        done_RCs = list(OUTDATA['REFCODE'])
+        logging.info(f'> {len(done_RCs)} structures already done.')
+    else:
+        # write output file
+        with open(output_file, 'w') as f:
+            f.write('REFCODE,molecule,pore_diam_opt,no_windows\n')
+        OUTDATA = pd.read_csv(output_file)
+        done_RCs = []
+
+    # iterate over CIFs
+    count = len(done_RCs)
+    for pdb in pdbs:
+        RC = pdb.replace('_extracted.pdb', '')
+        # skip done cifs
+        if RC in done_RCs:
+            continue
+        if os.path.isfile(pdb):
+            logging.info(f'> doing {count} of {len(RC)}: {RC}')
+            # load and modularize pdb
+            rbs = pywindow_f.modularize(file=pdb)
+            if rbs is None:
+                # handle pyWindow failure
+                raise(f'{pdb} failed modularize!')
+            # iterate over all molecules, skipping those with n_atoms < 5
+            mol_dict = {}
+            Mol = rbs.molecules
+            for molec in Mol:
+                mol = rbs.molecules[molec]
+                if mol.no_of_atoms < 5:
+                    continue
+                # run analysis
+                try:
+                    analysis = mol.full_analysis()
+                except ValueError:
+                    logging.warning(f'{pdb}_{molec} failed pywindow full_analysis.')
+                    analysis = None
+                # define output
+                if analysis is not None:
+                    pdo = analysis['pore_diameter_opt']['diameter']
+                    if analysis['windows']['diameters'] is not None:
+                        nwind = len(analysis['windows']['diameters'])
+                    else:
+                        nwind = 0
+                    mol_dict[molec] = (pdo, nwind)
+
+            # get molecule with largest pore diameter
+            pdos = [mol_dict[i][0] for i in mol_dict]
+            max_pdo = max(pdos)
+            max_molec = pdos.index(max_pdo)
+            max_nwind = mol_dict[max_molec][1]
+            # output structure
+            mol.dump_molecule(
+                RC + "_MP_{0}.pdb".format(max_molec),
+                include_coms=True,
+                override=True)
+            OUTDATA = OUTDATA.append({'REFCODE': RC, 'molecule': max_molec,
+                                      'pore_diam_opt': max_pdo,
+                                      'no_windows': max_nwind},
+                                     ignore_index=True)
+
+        else:
+            raise(f'{pdb} not present!')
+
+        # add to done cifs
+        done_RCs.append(RC)
+        # update output file
+        OUTDATA.to_csv(output_file, index=False)
+        count += 1
 
 
 if __name__ == "__main__":
