89563707658ef3228f9d4c2a866cc9a6072dd3e7
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-05-23 17:10:48 +0100

    planned out, but incomplete

--- cage_collect/classify_structures.py
+++ cage_collect/classify_structures.py
@@ -33,6 +33,74 @@ def main():
         DB_file = sys.argv[1]
         output_file = sys.argv[2]
 
+    refcodes = sorted([i.rstrip() for i in open(DB_file, 'r').readlines()])
+    rebuilt_pdbs = [i+'_extracted_rebuild.pdb' for i in refcodes]
+    logging.info(f'> started with: {len(refcodes)} structures to classify.')
+    if os.path.isfile(output_file):
+        # read CIFs already checked to avoid double calculations
+        OUTDATA = pd.read_csv(output_file)
+        done_RCs = list(OUTDATA['REFCODE'])
+        logging.info(f'> {len(done_RCs)} structures already done.')
+    else:
+        # write output file
+        with open(output_file, 'w') as f:
+            f.write('REFCODE,molecule,pore_diam_opt,no_windows,classification\n')
+        OUTDATA = pd.read_csv(output_file)
+        done_RCs = []
+
+    # iterate over CIFs
+    count = len(done_RCs)
+    for rbpdb in rebuilt_pdbs:
+        RC = rbpdb.replace('_extracted_rebuild.pdb', '')
+        # skip done cifs
+        if RC in done_RCs:
+            continue
+        if os.path.isfile(rbpdb):
+            logging.info(f'> doing {count} of {len(RC)}: {RC}')
+            # load in rebuilt structure
+
+            # modularize
+
+            # iterate over all molecules, skipping those with n_atoms < 5
+            for molec in MOLECULES:
+                # run analysis
+
+                # define output
+                pdo = 0
+                nwind = 0
+
+                # output structure
+
+                # visualize in PROGRAM to get class
+
+                CL = 'M'
+
+                # close program
+
+
+                OUTDATA = OUTDATA.append({'REFCODE': RC, 'molecule': molec,
+                                          'pore_diam_opt': pdo,
+                                          'no_windows': nwind,
+                                          'classification': CL},
+                                         ignore_index=True)
+
+        else:
+            # pdb missing.
+            molec = 0
+            pdo = 0
+            nwind = 0
+            CL = 'M'  # mistake
+            OUTDATA = OUTDATA.append({'REFCODE': RC, 'molecule': molec,
+                                      'pore_diam_opt': pdo,
+                                      'no_windows': nwind,
+                                      'classification': CL},
+                                     ignore_index=True)
+
+        # add to done cifs
+        done_RCs.append(RC)
+        # update output file
+        OUTDATA.to_csv(output_file, index=False)
+        count += 1
 
 
 if __name__ == "__main__":
