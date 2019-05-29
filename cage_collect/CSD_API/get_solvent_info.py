de9e1860d6dd12de108fe76fa8c82da30d05312d
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-05-29 15:06:56 +0100

    iterate over all PDB files in DB directory and collect solvent properties

--- cage_collect/CSD_API/get_solvent_info.py
+++ cage_collect/CSD_API/get_solvent_info.py
@@ -28,6 +28,52 @@ def main():
     else:
         file_suffix = sys.argv[1]
 
+    # read in CSD and updates
+    entry_reader = CSD_f.get_entryreader()
+
+    files = sorted(glob.glob('*'+file_suffix))
+    REFCODEs = []
+    for file in files:
+        REFCODEs.append(file.replace(file_suffix, ''))
+
+    RC_properties = []
+    for i, RC in enumerate(sorted(REFCODEs)):
+        print 'doing: '+str(RC)
+        entry = entry_reader.entry(RC)
+        # note structures with solvent
+        # solvent = 'n'
+        # if entry.chemical_name is not None:
+        #     if len(entry.chemical_name.split(' ')) > 1:
+        #         solvent = 'y'
+        # disorder details
+        squeeze = 'n'
+        disorder = 'n'
+        DD = entry.disorder_details
+        if DD is not None:
+            disorder = 'y'
+            if 'squeeze' in DD.lower():
+                squeeze = 'y'
+            elif 'platon' in DD.lower():
+                squeeze = 'y'
+            elif 'mask' in DD.lower():
+                squeeze = 'y'
+        print DD
+        rem = entry.remarks
+        print rem
+        RC_prop = (RC, squeeze, disorder)
+        print entry.chemical_name
+        print 'squeeze:'+squeeze
+        print 'disorder:'+disorder
+        print RC_prop
+        # sys.exit()
+        RC_properties.append(RC_prop)
+        # a = raw_input('')
+
+    # write to file
+    with open('solvent_prop.csv', 'w') as f:
+        for i in RC_properties:
+            f.write(i[0]+','+i[1]+','+i[2]+'\n')
+    print '-------------------------------------------------'
 
 
 if __name__ == "__main__":
