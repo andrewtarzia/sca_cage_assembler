cd8ae378ed50a8b094d88e9602a00c76d6d05d16
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-06-07 21:36:54 +0100

    finish rebase

--- README.md
+++
@@ -1,2 +0,0 @@
-# atools
-collection of packages for use in postdoc projects
--- atools/__init__.py
+++
@@ -1,9 +0,0 @@
-from .calculations import *
-from .GFN_f import *
-from .IO_tools import *
-from .plotting import *
-from .pywindow_f import *
-from .rdkit_f import *
-from .stk_f import *
-
-__version__ = '0.0.1'
