ef0efe01754917de0afb82050e5e99a1c8796a11
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-11-10 15:33:39 +0000

    prepare script for building prism topologies

---
+++ scripts/build_prism.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Distributed under the terms of the MIT License.
+
+import sys
+from os.path import exists
+import stk
+from rdkit.Chem import AllChem as Chem
+
+
+if __name__ == "__main__":
+    main()
