f0e242f8e3162aeef20e81d1597a0c7d92997b72
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-03-03 23:11:58 +0000

    add topologies and Cage class setup

--- scripts/cage_building.py
+++ scripts/cage_building.py
@@ -7,8 +7,32 @@ Modules defining and building the Cage class and subclasses.
 
 Author: Andrew Tarzia
 
-Date Created: 23 Jan 2020
+Date Created: 03 Mar 2020
+
 """
+
+from itertools import product
+from os.path import exists, join
+
+import stk
+
+
+def available_topologies(string):
+    """
+    Get stk function of desired topology.
+
+    """
+
+    topologies = {
+        'm4l4spacer': stk.cage.M4L4_Oct_Spacer(),
+        'm8l6face': stk.cage.M8L6_Oct_Face(),
+        'm6l2l3': stk.cage.M6L2L3_Oct()
+    }
+
+    try:
+        return string, topologies[string]
+    except KeyError:
+        raise KeyError(f'{string} not in {topologies.keys()}')
 
 
 class Cage:
@@ -17,8 +41,51 @@ class Cage:
 
     """
 
+    def __init__(self, name, bbs, topology, bb_vertices):
+        self.name = name
+        self.bbs = bbs
+        self.topology = topology
+        self.bb_vertices = bb_vertices
 
-class HetPrism(Cage):
+    def build(self):
+        print(f'....building {self.name}')
+        cage = stk.ConstructedMolecule(
+            building_blocks=self.bbs,
+            topology_graph=self.topology,
+            building_block_vertices=self.bb_vertices
+        )
+        cage.write(f'{self.name}_unopt.mol')
+        cage.dump(f'{self.name}_unopt.json')
+        self.cage = cage
+
+    def optimize(self):
+        if not exists(f'{self.name}_opt.json'):
+            print(f'....optimizing {self.name}')
+            raise NotImplementedError()
+
+    def get_energy(self):
+        print(f'....getting energy of {self.name}')
+        raise NotImplementedError()
+
+    def compare_UHF_values(self):
+        print(f'....comparing UHF of {self.name}')
+        raise NotImplementedError()
+
+    def analyze_cage_geometry(self):
+        print(f'....analyzing {self.name}')
+        raise NotImplementedError()
+
+    def __str__(self):
+        return (
+            f'{self.__class__.__name__}'
+            f'(name={self.name}, topology={self.topology})'
+        )
+
+    def __repr__(self):
+        return str(self)
+
+
+class HetPrism:
     """
     Class that builds and analyses stk.ConstructuedMolecules.
 
