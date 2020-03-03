260231fc04e0fc30d3da8195f89d1be2790472ce
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-03-03 18:10:00 +0000

    wip Cage classes

---
+++ scripts/Cage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Distributed under the terms of the MIT License.
+
+"""
+Modules defining the Cage class and subclasses.
+
+Author: Andrew Tarzia
+
+Date Created: 23 Jan 2020
+"""
+
+
+class Cage:
+    """
+    Generic class that builds and analyses stk.ConstructuedMolecules.
+
+    """
+
+
+class HetPrism(Cage):
+    """
+    Class that builds and analyses stk.ConstructuedMolecules.
+
+    Represents heteroleptic prism cages and all necessary homoleptic
+    cages.
+
+    """
