4132dee9c399b7f3d9043eb056a85a4b600e9551
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-03-15 18:20:27 +0000

    refactor into parent class for cage set -> homo cubes and het prisms

--- scripts/cage_building.py
+++ scripts/cage_building.py
@@ -236,27 +236,26 @@ class Cage:
         return str(self)
 
 
-class HetPrism:
+class CageSet:
     """
     Class that builds and analyses stk.ConstructuedMolecules.
-
-    Represents heteroleptic prism cages and all necessary homoleptic
-    cages.
 
     """
 
     def __init__(
         self,
         name,
-        prism_dict,
+        cage_dict,
         complex_dicts,
+        ligand_dicts,
         ligand_dir,
         complex_dir
     ):
         self.name = name
         self.properties_file = f'{self.name}_HP.json'
-        self.prism_dict = prism_dict
+        self.cage_dict = cage_dict
         self.complex_dicts = complex_dicts
+        self.ligand_dicts = ligand_dicts
         self.cages_to_build = self.define_cages_to_build(
             ligand_dir,
             complex_dir
@@ -289,7 +288,7 @@ class HetPrism:
     def __repr__(self):
         return str(self)
 
-    def get_ratios(self, n_metals):
+    def _get_ratios(self, n_metals):
         rng = range(0, n_metals+1)
         rats = []
         for i in product(rng, rng):
@@ -297,7 +296,7 @@ class HetPrism:
                 rats.append(i)
         return rats
 
-    def load_complex(self, complex_name, complex_dir):
+    def _load_complex(self, complex_name, complex_dir):
         unopt_c = stk.ConstructedMolecule.load(
             join(complex_dir, f'{complex_name}_opt.json')
         )
@@ -308,7 +307,7 @@ class HetPrism:
 
         return complex
 
-    def load_ligand(self, ligand_name, ligand_dir):
+    def _load_ligand(self, ligand_name, ligand_dir):
         unopt_l = stk.Molecule.load(
             join(ligand_dir, f'{ligand_name}_opt.json')
         )
@@ -317,11 +316,17 @@ class HetPrism:
             functional_groups=['bromine']
         )
 
-        with open(join(ligand_dir, 'ligand_data.json'), 'r') as f:
-            lig_prop = json.load(f)
-        properties = lig_prop[ligand_name]
+        return ligand
 
-        return ligand, properties
+
+class HoCube(CageSet):
+    """
+    Class that builds and analyses stk.ConstructuedMolecules.
+
+    Represents homoleptic cube cages with all necessary symmetries
+    and orientations.
+
+    """
 
     def define_cages_to_build(self, ligand_dir, complex_dir):
         """
@@ -340,36 +345,30 @@ class HetPrism:
             i for i in self.complex_dicts if 'lam' in i
         ][0]
         print(D_complex_name, L_complex_name)
-        L_complex = self.load_complex(
+        L_complex = self._load_complex(
             complex_name=L_complex_name,
             complex_dir=complex_dir
         )
-        D_complex = self.load_complex(
+        D_complex = self._load_complex(
             complex_name=D_complex_name,
             complex_dir=complex_dir
         )
 
         D_charge = self.complex_dicts[D_complex_name]['total_charge']
         L_charge = self.complex_dicts[L_complex_name]['total_charge']
-        D_free_e = self.complex_dicts[D_complex_name][
-            'unpaired_e'
-        ].strip(')()').split(',')
-        L_free_e = self.complex_dicts[L_complex_name][
-            'unpaired_e'
-        ].strip(')()').split(',')
+        D_free_e = self.complex_dicts[D_complex_name]['unpaired_e']
+        L_free_e = self.complex_dicts[L_complex_name]['unpaired_e']
         print(D_charge, D_free_e, L_charge, L_free_e)
 
         # Get all linkers.
-        tet_linker, tet_prop = self.load_ligand(
-            ligand_name=self.prism_dict['tetratopic'],
-            ligand_dir=ligand_dir
-        )
-        tri_linker, tri_prop = self.load_ligand(
-            ligand_name=self.prism_dict['tritopic'],
+        tet_prop = self.ligand_dicts[self.cage_dict['tetratopic']]
+        tet_linker = self._load_ligand(
+            ligand_name=self.cage_dict['tetratopic'],
             ligand_dir=ligand_dir
         )
 
-        print(D_complex, L_complex, tet_linker, tri_linker)
+        print(D_complex, L_complex, tet_linker)
+        print('tprop', tet_prop)
 
         # Tetratopic homoleptic cages of all symmetries.
         tet_topo_name, tet_topo = available_topologies(
@@ -377,12 +376,12 @@ class HetPrism:
         )
 
         tet_n_metals = 8
-        tet_ratios = self.get_ratios(tet_n_metals)
+        tet_ratios = self._get_ratios(tet_n_metals)
         for rat in tet_ratios:
             print(rat)
             new_name = (
-                f"C_{self.prism_dict['corner_name']}_"
-                f"{self.prism_dict['tetratopic']}_"
+                f"C_{self.cage_dict['corner_name']}_"
+                f"{self.cage_dict['tetratopic']}_"
                 f"{tet_topo_name}_"
                 f"d{rat[0]}l{rat[1]}_"
                 f"SYMM"
@@ -395,7 +394,174 @@ class HetPrism:
             }
             # Merge linker and complex charges.
             complex_charge = rat[0]*int(D_charge)+rat[1]*int(L_charge)
-            new_charge = int(tet_prop[0])*6 + complex_charge
+            new_charge = tet_prop['net_charge']*6 + complex_charge
+
+            print(tet_prop['total_unpaired_e'])
+            lig_free_e = [
+                int(i)*6 for i in tet_prop['total_unpaired_e']
+            ]
+            compl_free_e = [
+                int(i)*rat[0] + int(j)*rat[1]
+                for i, j in zip(D_free_e, L_free_e)
+            ]
+            print(lig_free_e, compl_free_e)
+
+            new_free_electron_options = []
+            for opt in product(lig_free_e, compl_free_e):
+                print(opt)
+                new_free_electron_options.append(opt[0]+opt[1])
+
+            print(new_charge, new_free_electron_options)
+            new_cage = Cage(
+                name=new_name,
+                bbs=new_bbs,
+                topology=tet_topo,
+                topology_string=tet_topo_name,
+                bb_vertices=new_bb_vertices,
+                charge=new_charge,
+                free_electron_options=new_free_electron_options
+            )
+            cages_to_build.append(new_cage)
+            print('NOT BUILDING ALL RATIOS CURRENTLY!!!!!!')
+            break
+
+        return cages_to_build
+
+    def plot_min_OPs_avg_PV(self, X, Y, T):
+        topo_c_m = {
+            'm4l4spacer': ('#E074AF', 'o', r'M$_4$L$_4$'),
+            'm8l6face': ('#AFE074', 'X', r'M$_8$L$_6$'),
+            'm6l2l3': ('#74AFE0', 'P', r'M$_6$L$^a_2$L$^b_3$')
+        }
+
+        Cs = [
+            topo_c_m[C.topology_string][0]
+            for C in self.cages_to_build
+        ]
+        print(len(X), len(Y), len(Cs))
+
+        fig, ax = plt.subplots(figsize=(8, 5))
+        for x, y, t in zip(X, Y, T):
+            ax.scatter(
+                x,
+                y,
+                c=topo_c_m[t][0],
+                edgecolors='k',
+                marker=topo_c_m[t][1],
+                alpha=1.0,
+                s=80
+            )
+        # Set number of ticks for x-axis
+        ax.tick_params(axis='both', which='major', labelsize=16)
+        ax.set_xlabel(r'pore volume [$\mathrm{\AA}^3$]', fontsize=16)
+        ax.set_ylabel(r'min. $q_{\mathrm{oct}}$', fontsize=16)
+        ax.set_xlim(0, 2000)
+        ax.set_ylim(0, 1)
+
+        # Implement legend.
+        for i in topo_c_m:
+            ax.scatter(
+                -1000,
+                -1000,
+                c=topo_c_m[i][0],
+                edgecolors='k',
+                marker=topo_c_m[i][1],
+                alpha=1.0,
+                s=80,
+                label=topo_c_m[i][2]
+            )
+
+        ax.legend(fontsize=16)
+
+        fig.tight_layout()
+        fig.savefig(
+            f'{self.name}_minOPsVSporevol.pdf',
+            dpi=720,
+            bbox_inches='tight'
+        )
+        plt.close()
+
+
+class HetPrism(CageSet):
+    """
+    Class that builds and analyses stk.ConstructuedMolecules.
+
+    Represents heteroleptic prism cages and all necessary homoleptic
+    cages.
+
+    """
+
+    def define_cages_to_build(self, ligand_dir, complex_dir):
+        """
+        Defines the name and objects of all cages to build.
+
+        """
+
+        cages_to_build = []
+
+        # Get Delta and Lambda complex.
+        print(self.complex_dicts)
+        D_complex_name = [
+            i for i in self.complex_dicts if 'del' in i
+        ][0]
+        L_complex_name = [
+            i for i in self.complex_dicts if 'lam' in i
+        ][0]
+        print(D_complex_name, L_complex_name)
+        L_complex = self._load_complex(
+            complex_name=L_complex_name,
+            complex_dir=complex_dir
+        )
+        D_complex = self._load_complex(
+            complex_name=D_complex_name,
+            complex_dir=complex_dir
+        )
+
+        D_charge = self.complex_dicts[D_complex_name]['total_charge']
+        L_charge = self.complex_dicts[L_complex_name]['total_charge']
+        D_free_e = self.complex_dicts[D_complex_name]['unpaired_e']
+        L_free_e = self.complex_dicts[L_complex_name]['unpaired_e']
+        print(D_charge, D_free_e, L_charge, L_free_e)
+
+        # Get all linkers.
+        tet_prop = self.ligand_dicts[self.cage_dict['tetratopic']]
+        tet_linker = self._load_ligand(
+            ligand_name=self.cage_dict['tetratopic'],
+            ligand_dir=ligand_dir
+        )
+        tri_prop = self.ligand_dicts[self.cage_dict['tritopic']]
+        tri_linker = self._load_ligand(
+            ligand_name=self.cage_dict['tritopic'],
+            ligand_dir=ligand_dir
+        )
+
+        print(D_complex, L_complex, tet_linker, tri_linker)
+
+        # Tetratopic homoleptic cages of all symmetries.
+        tet_topo_name, tet_topo = available_topologies(
+            string='m8l6face'
+        )
+
+        tet_n_metals = 8
+        tet_ratios = self._get_ratios(tet_n_metals)
+        for rat in tet_ratios:
+            print(rat)
+            new_name = (
+                f"C_{self.cage_dict['corner_name']}_"
+                f"{self.cage_dict['tetratopic']}_"
+                f"{tet_topo_name}_"
+                f"d{rat[0]}l{rat[1]}_"
+                f"SYMM"
+            )
+            new_bbs = [D_complex, L_complex, tet_linker]
+            new_bb_vertices = {
+                D_complex: tet_topo.vertices[:rat[0]],
+                L_complex: tet_topo.vertices[rat[0]:rat[0]+rat[1]],
+                tet_linker: tet_topo.vertices[tet_n_metals:]
+            }
+            # Merge linker and complex charges.
+            complex_charge = rat[0]*int(D_charge)+rat[1]*int(L_charge)
+            new_charge = tet_prop['net_charge']*6 + complex_charge
 
             print(tet_prop[1])
             lig_free_e = [int(i)*6 for i in tet_prop[1]]
@@ -430,12 +596,12 @@ class HetPrism:
         )
 
         tri_n_metals = 4
-        tri_ratios = self.get_ratios(tri_n_metals)
+        tri_ratios = self._get_ratios(tri_n_metals)
         for rat in tri_ratios:
             print(rat)
             new_name = (
-                f"C_{self.prism_dict['corner_name']}_"
-                f"{self.prism_dict['tritopic']}_"
+                f"C_{self.cage_dict['corner_name']}_"
+                f"{self.cage_dict['tritopic']}_"
                 f"{tri_topo_name}_"
                 f"d{rat[0]}l{rat[1]}_"
                 f"SYMM"
@@ -448,10 +614,12 @@ class HetPrism:
             }
             # Merge linker and complex charges.
             complex_charge = rat[0]*int(D_charge)+rat[1]*int(L_charge)
-            new_charge = int(tri_prop[0])*4 + complex_charge
+            new_charge = tri_prop['net_charge']*4 + complex_charge
 
-            print(tri_prop[1])
-            lig_free_e = [int(i)*4 for i in tri_prop[1]]
+            print(tri_prop['total_unpaired_e'])
+            lig_free_e = [
+                int(i)*4 for i in tri_prop['total_unpaired_e']
+            ]
             compl_free_e = [
                 int(i)*rat[0] + int(j)*rat[1]
                 for i, j in zip(D_free_e, L_free_e)
@@ -483,12 +651,12 @@ class HetPrism:
         )
 
         pri_n_metals = 6
-        pri_ratios = self.get_ratios(pri_n_metals)
+        pri_ratios = self._get_ratios(pri_n_metals)
         for rat in pri_ratios:
             new_name = (
-                f"HeP_{self.prism_dict['corner_name']}_"
-                f"{self.prism_dict['tritopic']}_"
-                f"{self.prism_dict['tetratopic']}_"
+                f"HeP_{self.cage_dict['corner_name']}_"
+                f"{self.cage_dict['tritopic']}_"
+                f"{self.cage_dict['tetratopic']}_"
                 f"{pri_topo_name}_"
                 f"d{rat[0]}l{rat[1]}_"
                 f"SYMM"
@@ -504,12 +672,19 @@ class HetPrism:
             }
             # Merge linker and complex charges.
             complex_charge = rat[0]*int(D_charge)+rat[1]*int(L_charge)
-            new_charge = int(tri_prop[0])*2
-            new_charge += int(tet_prop[0])*3 + complex_charge
+            new_charge = tri_prop['net_charge']*2
+            new_charge += tet_prop['net_charge']*3 + complex_charge
 
-            print(tri_prop[1], tet_prop[1])
-            tri_free_e = [int(i)*2 for i in tri_prop[1]]
-            tet_free_e = [int(i)*3 for i in tet_prop[1]]
+            print(
+                tri_prop['total_unpaired_e'],
+                tet_prop['total_unpaired_e']
+            )
+            tri_free_e = [
+                int(i)*2 for i in tri_prop['total_unpaired_e']
+            ]
+            tet_free_e = [
+                int(i)*3 for i in tet_prop['total_unpaired_e']
+            ]
             compl_free_e = [
                 int(i)*rat[0] + int(j)*rat[1]
                 for i, j in zip(D_free_e, L_free_e)
