487708f53b1653a94158cd9d4a919ce4be402252
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-07-06 18:03:57 +0100

    Minor

--- scripts/cage_building.py
+++ scripts/cage_building.py
@@ -23,7 +23,10 @@ import stk
 import atools
 from molecule_building import metal_FFs
 import symmetries
-from utilities import calculate_binding_AR
+from utilities import (
+    calculate_binding_AR,
+    calculate_paired_face_anisotropies
+)
 
 
 class UnexpectedNumLigands(Exception):
@@ -102,6 +105,65 @@ def available_topologies(string):
         'm4l4spacer': m4l4spacer_graph,
         'm8l6face': m8l6_graph,
         'm6l2l3': m6l2l3_graph,
+    }
+
+    try:
+        return topologies[string]
+    except KeyError:
+        raise KeyError(f'{string} not in {topologies.keys()}')
+
+
+def defined_face_sets(string):
+    """
+    Define which vertices in a topology share a face.
+
+    """
+
+    topologies = {
+        'm8l6face': {
+            '001': {
+                'vertices': (0, 1, 2, 3),
+                'connected': (
+                    (0, 1), (1, 2), (2, 3), (3, 0)
+                ),
+                'opposite': '00-1'
+            },
+            '100': {
+                'vertices': (0, 1, 4, 5),
+                'connected': (
+                    (0, 1), (1, 5), (5, 4), (4, 0)
+                ),
+                'opposite': '-100'
+            },
+            '010': {
+                'vertices': (0, 3, 4, 7),
+                'connected': (
+                    (0, 3), (3, 7), (7, 4), (4, 0)
+                ),
+                'opposite': '0-10'
+            },
+            '00-1': {
+                'vertices': (4, 5, 6, 7),
+                'connected': (
+                    (4, 5), (5, 6), (6, 7), (7, 4)
+                ),
+                'opposite': '001'
+            },
+            '-100': {
+                'vertices': (2, 3, 6, 7),
+                'connected': (
+                    (2, 3), (3, 7), (7, 6), (6, 2)
+                ),
+                'opposite': '100'
+            },
+            '0-10': {
+                'vertices': (1, 2, 5, 6),
+                'connected': (
+                    (1, 2), (2, 6), (6, 5), (5, 1)
+                ),
+                'opposite': '010'
+            },
+        },
     }
 
     try:
@@ -411,9 +473,9 @@ class Cage:
 
         """
 
+        print(f'....analyzing metal geometry of {self.name}')
         # Check if output file exists.
         if not exists(f'{self.op_file}.json'):
-            print(f'....analyzing metal geometry of {self.name}')
 
             # Get atomic numbers of all present metals.
             pres_atm_no = list(set([
@@ -439,6 +501,24 @@ class Cage:
         # Get data.
         with open(f'{self.op_file}.json', 'r') as f:
             self.op_data = json.load(f)
+
+        # Get metal-metal distances and face anisotropies.
+        # Assumes that atom ordering of metals follows vertex ordering
+        # in topology definition.
+
+        # Get metal atom ids.
+        metal_atom_ids = [
+            i.get_id() for i in self.cage.get_atoms()
+            if i.get_atomic_number() in metal_FFs(CN=4).keys()
+        ]
+
+        face_sets = defined_face_sets(self.topology_string)
+
+        self.fa_data = calculate_paired_face_anisotropies(
+            mol=self.cage,
+            metal_atom_ids=metal_atom_ids,
+            face_sets=face_sets,
+        )
 
     def analyze_porosity(self, dump_molecule=False):
         """
