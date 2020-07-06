52381223b4117c2fffee94e19510f72dabb6f9ac
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-07-06 20:00:24 +0100

    Convert CageSet plot to heatmap.

--- scripts/build_cage_library.py
+++ scripts/build_cage_library.py
@@ -11,43 +11,230 @@ Date Created: 27 Jan 2020
 """
 
 import sys
+from os.path import exists
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
 import cage_building
 from utilities import read_lib
 
 
-def plot_set_Y_vs_aniso(data, Y_name, ylabel, ylim, filename):
-    C = '#AFE074'
-    M = 'o'
+def heatmap(
+    data,
+    row_labels,
+    col_labels,
+    ax=None,
+    cbar_kw={},
+    cbarlabel="",
+    **kwargs
+):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
 
-    fig, ax = plt.subplots(figsize=(8, 5))
-    for name in data:
-        X = data[name]['aspect_ratio']
-        # Iterate over all cages in set.
-        for Y_d in data[name][Y_name]:
-            Y = data[name][Y_name][Y_d]
-            print(name, X, Y)
-            print(data[name])
-            print(data[name][Y_name])
-            input()
-            ax.scatter(
-                X,
-                Y,
-                c=C,
-                edgecolors='k',
-                marker=M,
-                alpha=1.0,
-                s=120
+    From: https://matplotlib.org/3.1.1/gallery/
+    images_contours_and_fields/image_annotated_heatmap.html
+    #sphx-glr-gallery-images-contours-and-fields-image-
+    annotated-heatmap-py
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (N, M).
+    row_labels
+        A list or array of length N with the labels for the rows.
+    col_labels
+        A list or array of length M with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is
+        plotted.  If not provided, use current axes or create a new
+        one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.
+        Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+
+    """
+
+    if not ax:
+        ax = plt.gca()
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(data.shape[1]))
+    ax.set_yticks(np.arange(data.shape[0]))
+    # ... and label them with the respective list entries.
+    ax.set_xticklabels(col_labels)
+    ax.set_yticklabels(row_labels)
+
+    # Let the horizontal axes labeling appear on top.
+    ax.tick_params(
+        top=True,
+        bottom=False,
+        labeltop=True,
+        labelbottom=False
+    )
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(
+        ax.get_xticklabels(),
+        rotation=-30,
+        ha="right",
+        rotation_mode="anchor"
+    )
+
+    # Turn spines off and create white grid.
+    for edge, spine in ax.spines.items():
+        spine.set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    return im, cbar
+
+
+def annotate_heatmap(
+    im,
+    data=None,
+    valfmt="{x:.2f}",
+    textcolors=["black", "white"],
+    threshold=None,
+    na_points=None,
+    **textkw
+):
+    """
+    A function to annotate a heatmap.
+
+    From: https://matplotlib.org/3.1.1/gallery/
+    images_contours_and_fields/image_annotated_heatmap.html
+    #sphx-glr-gallery-images-contours-and-fields-image-
+    annotated-heatmap-py
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.
+        Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should
+        either use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A list or array of two color specifications.  The first is used
+        for values below a threshold, the second for those above.
+        Optional.
+    threshold
+        Value in data units according to which the colors from
+        textcolors are applied.  If None (the default) uses the middle
+        of the colormap as separation.  Optional.
+    na_points
+        Tuple of (row, column) indices whose text should be manually
+        set to N/A. Optional
+    **kwargs
+        All other arguments are forwarded to each call to `text` used
+        to create the text labels.
+
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max())/2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(
+        horizontalalignment="center",
+        verticalalignment="center"
+    )
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(
+                color=textcolors[int(im.norm(data[i, j]) > threshold)]
             )
-    # Set number of ticks for x-axis
-    ax.tick_params(axis='both', which='major', labelsize=16)
-    ax.set_xlabel('ligand aspect ratio [1:X]', fontsize=16)
-    ax.set_ylabel(ylabel, fontsize=16)
-    ax.set_xlim(1, 10)
-    ax.set_ylim(ylim)
+            cond1 = na_points is not None
+            cond2 = False if na_points is None else (i, j) in na_points
+            if cond1 and cond2:
+                text = im.axes.text(j, i, 'N/A', **kw)
+            else:
+                text = im.axes.text(
+                    j,
+                    i,
+                    valfmt(data[i, j], None),
+                    **kw
+                )
+            texts.append(text)
 
+    return texts
+
+
+def plot_set_heatmap_vs_aniso(data, Y_name, ylabel, ylim, filename):
+
+    symmetries = [
+        'o1', 'th1', 'th2', 't1', 's61', 's62', 'd31', 'd32',
+        'c2v', 'c2h',
+    ]
+    col_names = {i: j['aspect_ratio'] for i, j in data.items()}
+    col_names = {
+        i: j for i, j in sorted(
+            col_names.items(), key=lambda item: item[1]
+        )
+    }
+    aspect_ratios = sorted(col_names.values())
+    out_values = np.zeros((len(symmetries), len(aspect_ratios)))
+
+    fig, ax = plt.subplots()
+
+    for name in data:
+        col = list(col_names.keys()).index(name)
+        # Iterate over all cages in set.
+        for cage in data[name][Y_name]:
+            Y = data[name][Y_name][cage]
+            symm = cage.split('_')[-1]
+            row = symmetries.index(symm)
+
+            # Assign to zeros matrix.
+            if out_values[row][col] != 0:
+                raise ValueError()
+
+            out_values[row][col] = Y
+
+    im, cbar = heatmap(
+        data=out_values,
+        row_labels=symmetries,
+        col_labels=[f'{round(i, 2)}' for i in col_names.values()],
+        ax=ax,
+        cmap='OrRd',
+        cbarlabel=ylabel
+    )
+    texts = annotate_heatmap(im, valfmt="{x:.2f}")
+    ax.set_xlabel('ligand aspect ratio [1:X]')
     fig.tight_layout()
     fig.savefig(
         filename,
@@ -270,11 +457,11 @@ def analyse_cages(cage_sets):
     }
     if len(AR_data) > 0:
         for t in tests:
-            plot_set_Y_vs_aniso(
+            plot_set_heatmap_vs_aniso(
                 data=AR_data,
                 Y_name=t,
                 ylabel=tests[t][0],
-                ylim=tests[t][0],
+                ylim=tests[t][1],
                 filename=f'plotset_{t}_VA.pdf'
             )
 
@@ -284,7 +471,7 @@ def build_cages(
     complexes,
     cage_lib,
     ligand_directory,
-    complex_directory
+    complex_directory,
     read_data,
 ):
 
