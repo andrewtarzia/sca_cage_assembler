ff0469df924be9ea37ed10a001544d77ceb4d80c
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-08-09 20:16:36 +0100

    Some refactoring

--- scripts/build_cage_library.py
+++ scripts/build_cage_library.py
@@ -15,183 +15,9 @@ from itertools import combinations
 from os.path import exists
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
 
-import cage_building
-from utilities import read_lib
-
-
-def heatmap(
-    data,
-    row_labels,
-    col_labels,
-    ax=None,
-    cbar_kw={},
-    cbarlabel="",
-    **kwargs
-):
-    """
-    Create a heatmap from a numpy array and two lists of labels.
-
-    From: https://matplotlib.org/3.1.1/gallery/
-    images_contours_and_fields/image_annotated_heatmap.html
-    #sphx-glr-gallery-images-contours-and-fields-image-
-    annotated-heatmap-py
-
-    Parameters
-    ----------
-    data
-        A 2D numpy array of shape (N, M).
-    row_labels
-        A list or array of length N with the labels for the rows.
-    col_labels
-        A list or array of length M with the labels for the columns.
-    ax
-        A `matplotlib.axes.Axes` instance to which the heatmap is
-        plotted.  If not provided, use current axes or create a new
-        one.  Optional.
-    cbar_kw
-        A dictionary with arguments to `matplotlib.Figure.colorbar`.
-        Optional.
-    cbarlabel
-        The label for the colorbar.  Optional.
-    **kwargs
-        All other arguments are forwarded to `imshow`.
-
-    """
-
-    if not ax:
-        ax = plt.gca()
-
-    # Plot the heatmap
-    im = ax.imshow(data, **kwargs)
-
-    # Create colorbar
-    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
-    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
-
-    # We want to show all ticks...
-    ax.set_xticks(np.arange(data.shape[1]))
-    ax.set_yticks(np.arange(data.shape[0]))
-    # ... and label them with the respective list entries.
-    ax.set_xticklabels(col_labels)
-    ax.set_yticklabels(row_labels)
-
-    # Let the horizontal axes labeling appear on top.
-    ax.tick_params(
-        top=True,
-        bottom=False,
-        labeltop=True,
-        labelbottom=False
-    )
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(
-        ax.get_xticklabels(),
-        rotation=-30,
-        ha="right",
-        rotation_mode="anchor"
-    )
-
-    # Turn spines off and create white grid.
-    for edge, spine in ax.spines.items():
-        spine.set_visible(False)
-    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
-    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
-    # ax.grid(which="minor", color="k", linestyle='-', linewidth=2)
-    ax.tick_params(which="minor", bottom=False, left=False)
-
-    return im, cbar
-
-
-def annotate_heatmap(
-    im,
-    data=None,
-    valfmt="{x:.2f}",
-    textcolors=["black", "white"],
-    threshold=None,
-    na_points=None,
-    **textkw
-):
-    """
-    A function to annotate a heatmap.
-
-    From: https://matplotlib.org/3.1.1/gallery/
-    images_contours_and_fields/image_annotated_heatmap.html
-    #sphx-glr-gallery-images-contours-and-fields-image-
-    annotated-heatmap-py
-
-    Parameters
-    ----------
-    im
-        The AxesImage to be labeled.
-    data
-        Data used to annotate.  If None, the image's data is used.
-        Optional.
-    valfmt
-        The format of the annotations inside the heatmap.  This should
-        either use the string format method, e.g. "$ {x:.2f}", or be a
-        `matplotlib.ticker.Formatter`.  Optional.
-    textcolors
-        A list or array of two color specifications.  The first is used
-        for values below a threshold, the second for those above.
-        Optional.
-    threshold
-        Value in data units according to which the colors from
-        textcolors are applied.  If None (the default) uses the middle
-        of the colormap as separation.  Optional.
-    na_points
-        Tuple of (row, column) indices whose text should be manually
-        set to N/A. Optional
-    **kwargs
-        All other arguments are forwarded to each call to `text` used
-        to create the text labels.
-
-    """
-
-    if not isinstance(data, (list, np.ndarray)):
-        data = im.get_array()
-
-    # Normalize the threshold to the images color range.
-    if threshold is not None:
-        threshold = im.norm(threshold)
-    else:
-        threshold = im.norm(data.max())/2.
-
-    # Set default alignment to center, but allow it to be
-    # overwritten by textkw.
-    kw = dict(
-        horizontalalignment="center",
-        verticalalignment="center"
-    )
-    kw.update(textkw)
-
-    # Get the formatter in case a string is supplied
-    if isinstance(valfmt, str):
-        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
-
-    # Loop over the data and create a `Text` for each "pixel".
-    # Change the text's color depending on the data.
-    texts = []
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            kw.update(
-                color=textcolors[int(im.norm(data[i, j]) > threshold)]
-            )
-            cond1 = na_points is not None
-            cond2 = False if na_points is None else (i, j) in na_points
-            if cond1 and cond2:
-                text = im.axes.text(j, i, 'N/A', **kw)
-            else:
-                text = im.axes.text(
-                    j,
-                    i,
-                    valfmt(data[i, j], None),
-                    **kw
-                )
-            texts.append(text)
-
-    return texts
+from cage_set import HoCube, HetPrism
+from utilities import read_lib, heatmap, annotate_heatmap
 
 
 def plot_set_heatmap_vs_aniso(data, Y_name, ylabel, ylim, filename):
@@ -233,7 +59,7 @@ def plot_set_heatmap_vs_aniso(data, Y_name, ylabel, ylim, filename):
         cmap='Blues',
         cbarlabel=ylabel
     )
-    texts = annotate_heatmap(im, valfmt="{x:.1f}", fontsize=8)
+    annotate_heatmap(im, valfmt="{x:.1f}", fontsize=8)
     ax.set_xlabel('ligand aspect ratio [1:X]')
     fig.tight_layout()
     fig.savefig(
@@ -427,7 +253,7 @@ def homo_cube_analysis(cage_set):
         'max_ML_length': {
             'data': measures['max_ML_length'],
             'ylabel': r'max. N-Zn bond length [$\mathrm{\AA}$]',
-            'ylim': (2, 4),
+            'ylim': (2, 3),
             'filename': f'{cage_set.name}_maxmld.pdf'
         },
         'relfe': {
@@ -439,7 +265,7 @@ def homo_cube_analysis(cage_set):
                 for i in measures['formation_energies']
             },
             'ylabel': r'rel. formation energy [kJ/mol]',
-            'ylim': (-10, 10000),
+            'ylim': (-10, 1000),
             'filename': f'{cage_set.name}_relfe.pdf'
         },
     }
@@ -492,9 +318,9 @@ def analyse_cages(cage_sets):
 
     AR_data = {}
     for cage_set in cage_sets:
-        if isinstance(cage_set, cage_building.HetPrism):
+        if isinstance(cage_set, HetPrism):
             het_prism_analysis(cage_set)
-        if isinstance(cage_set, cage_building.HoCube):
+        if isinstance(cage_set, HoCube):
             measures = homo_cube_analysis(cage_set)
             # For all HoCube sets, collect ligand aspect ratio data.
             AR_data[cage_set.name] = {
@@ -533,9 +359,9 @@ def analyse_cages(cage_sets):
             r'max. $\Delta$opposing face anisotropy [%]', (-10, 100)
         ),
         'max_mld': (
-            r'max. N-Zn bond length [$\mathrm{\AA}$]', (2, 4)
+            r'max. N-Zn bond length [$\mathrm{\AA}$]', (2, 3)
         ),
-        'rel_fe': (r'rel. formation energy [kJ/mol]', (-10, 10000)),
+        'rel_fe': (r'rel. formation energy [kJ/mol]', (-10, 1000)),
     }
     if len(AR_data) > 0:
         for t in tests:
@@ -564,7 +390,7 @@ def build_cages(
         comps = {i: complexes[i] for i in compl_names}
 
         if cage_set_c['heteroleptic']:
-            cage_set = cage_building.HetPrism(
+            cage_set = HetPrism(
                 name=name,
                 cage_set_dict=cage_set_c,
                 complex_dicts=comps,
@@ -573,7 +399,7 @@ def build_cages(
                 complex_dir=complex_directory
             )
         else:
-            cage_set = cage_building.HoCube(
+            cage_set = HoCube(
                 name=name,
                 cage_set_dict=cage_set_c,
                 complex_dicts=comps,
@@ -631,7 +457,7 @@ def build_cages(
                     'fe_prop': C.fe_data,
                     'li_prop': C.ls_data,
                     'fa_prop': C.fa_data,
-                    'bl_data': C.bl_data
+                    'bl_prop': C.bl_data
                 }
                 # Dump to JSON.
                 cage_set.dump_properties()
