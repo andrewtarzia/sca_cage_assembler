0b320b63312ed7abaef556c6f0e59eec90a5c477
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-01-23 12:55:50 +0000

    Clean up script.

--- scripts/extract_cage.py
+++ scripts/extract_cage.py
@@ -8,12 +8,14 @@ Script to extract cage molecule from CIF.
 Author: Andrew Tarzia
 
 Date Created: 15 Mar 2019
+
 """
+
 import sys
 import atools
 
 
-if __name__ == "__main__":
+def main():
     if (not len(sys.argv) == 2):
         print("""
 Usage: extract_cage.py CIF
@@ -22,22 +24,27 @@ Usage: extract_cage.py CIF
         sys.exit()
     else:
         file = sys.argv[1]
+
     file_prefix = file.replace('.cif', '')
     pdb_file, ASE_structure = atools.convert_CIF_2_PDB(file)
     if pdb_file is None and ASE_structure is None:
         sys.exit()
-    # rebuild system
+
+    # Rebuild system.
     rebuilt_structure = atools.modularize(file=pdb_file)
     if rebuilt_structure is None:
-        # handle pyWindow failure
+        # Handle pyWindow failure.
         sys.exit(f'pyWindow failure on {pdb_file}')
+
     for molecule in rebuilt_structure.molecules:
         print('Analysing molecule {0} out of {1}'.format(
-            molecule + 1, len(rebuilt_structure.molecules)))
+            molecule + 1, len(rebuilt_structure.molecules))
+        )
         mol = rebuilt_structure.molecules[molecule]
         # skip very small structures -- likely ions
         if mol.no_of_atoms < 20:
             continue
+
         print(mol.full_analysis(), '\n')
         # Each molecule can be saved separately
         pdb_output = file_prefix + "_{0}.pdb".format(molecule)
@@ -46,3 +53,7 @@ Usage: extract_cage.py CIF
             include_coms=False,
             override=True)
         _, _ = atools.convert_PDB_2_XYZ(pdb_output)
+
+
+if __name__ == "__main__":
+    main()
