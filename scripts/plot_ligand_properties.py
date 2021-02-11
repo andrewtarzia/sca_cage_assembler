a62059f0f435123e58bb09e130ea1b23cdc16f81
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-02-11 16:19:50 +0000

    Refactor, add new plot.

--- scripts/plot_ligand_properties.py
+++ scripts/plot_ligand_properties.py
@@ -11,36 +11,15 @@ Date Created: 08 Nov 2020
 
 """
 
-import numpy as np
 from glob import glob
 import json
 import sys
 import matplotlib.pyplot as plt
 
 from atools import colors_i_like
-from stk.molecular.atoms.elements import Y
 
 
-def main():
-    first_line = (
-        'Usage: plot_ligand_properties.py'
-    )
-    if (not len(sys.argv) == 1):
-        print(f"""
-{first_line}
-
-    """)
-        sys.exit()
-
-    json_files = glob('*_ligand_measures.json')
-
-    candms = {
-        '1': (colors_i_like()[9], 'o'),
-        '2': (colors_i_like()[10], 'X'),
-        '3': (colors_i_like()[11], 's'),
-        '4': (colors_i_like()[4], 'D'),
-        '5': (colors_i_like()[3], 'P'),
-    }
+def plot_all_ligand_properties(json_files, candms):
     fig, ax = plt.subplots(figsize=(8, 5))
     for i in json_files:
         cage_set = i.replace('_ligand_measures.json', '')
@@ -67,7 +46,6 @@ def main():
             alpha=1.0,
             s=120
         )
-
 
     for cm in candms:
         c, m = candms[cm]
@@ -97,6 +75,92 @@ def main():
     )
 
     plt.close()
+
+
+def plot_MM_vs_AR(json_files, candms):
+    fig, ax = plt.subplots(figsize=(8, 5))
+    stabs = {
+        '1': {'ar': [], 'stab': []},
+        '2': {'ar': [], 'stab': []},
+        '3': {'ar': [], 'stab': []},
+        '4': {'ar': [], 'stab': []},
+        '5': {'ar': [], 'stab': []},
+    }
+    for i in json_files:
+        cage_set = i.replace('_ligand_measures.json', '')
+        print(f'doing {cage_set}')
+        with open(i, 'r') as f:
+            cage_set_data = json.load(f)
+        for face in cage_set_data['face_properties']:
+            stabs[face]['ar'].append(
+                cage_set_data['ligand_aspect_ratio']
+            )
+            stabs[face]['stab'].append(
+                cage_set_data['face_properties'][face]
+            )
+
+    for face in stabs:
+        if face in ['1', '4', '5']:
+            continue
+        c, m = candms[face]
+        XY = [
+            (y, x) for y, x
+            in sorted(
+                zip(stabs[face]['ar'], stabs[face]['stab']),
+                key=lambda pair: pair[0]
+            )
+        ]
+        X = [i[0] for i in XY]
+        Y = [i[1] for i in XY]
+        ax.plot(
+            X,
+            Y,
+            c=c,
+            # edgecolors='k',
+            marker=m,
+            alpha=1.0,
+            markersize=10,
+            linestyle='dashed',
+            label=f'face: {face}'
+        )
+
+    ax.tick_params(axis='both', which='major', labelsize=16)
+    ax.set_xlabel('aspect ratio [1:X]', fontsize=16)
+    ax.set_ylabel('avg. side mismatch [%]', fontsize=16)
+    ax.set_xlim(1.0, 2.6)
+    ax.set_ylim(0, 110)
+    ax.legend(fontsize=16, ncol=2)
+    fig.savefig(
+        'all_ligand_MM_vs_AR.pdf',
+        dpi=720,
+        bbox_inches='tight'
+    )
+
+    plt.close()
+
+
+def main():
+    first_line = (
+        'Usage: plot_ligand_properties.py'
+    )
+    if (not len(sys.argv) == 1):
+        print(f"""
+{first_line}
+
+    """)
+        sys.exit()
+
+    json_files = glob('*_ligand_measures.json')
+
+    candms = {
+        '1': (colors_i_like()[9], 'o'),
+        '2': (colors_i_like()[4], 'X'),
+        '3': (colors_i_like()[11], 's'),
+        '4': (colors_i_like()[10], 'D'),
+        '5': (colors_i_like()[3], 'P'),
+    }
+    plot_all_ligand_properties(json_files, candms)
+    plot_MM_vs_AR(json_files, candms)
 
 
 if __name__ == "__main__":
