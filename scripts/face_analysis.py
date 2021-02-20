bdad92ca76516d31da6062fa77475f882ec4e1ad
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-02-20 18:59:27 +0000

    Add paths and fix extraction of mismatches. Add plots.

--- scripts/face_analysis.py
+++ scripts/face_analysis.py
@@ -14,13 +14,14 @@ Date Created: 19 Oct 2020
 import sys
 import json
 from os.path import exists, join
-from itertools import combinations
 import matplotlib.pyplot as plt
 from glob import glob
 import numpy as np
 
 import stk
 import stko
+
+from atools import get_query_atom_ids
 
 from molecule_building import metal_FFs
 from cubeface import CubeFace
@@ -29,7 +30,6 @@ from atools import (
     MOC_collapse_mc,
     MOC_uff_opt,
     get_atom_distance,
-    colors_i_like,
 )
 
 
@@ -178,56 +178,125 @@ def get_all_bond_lengths(face):
 
     return all_bls
 
+
+def calculate_face_properties(face, paths, metal_atomic_number=30):
     """
-    Calculate geoemtrical properties of a face.
+    Calculate geometrical properties of a face.
 
     """
 
-    # Get metal atom ids.
-    metal_atom_ids = []
-    for atom in face.get_atoms():
-        if atom.get_atomic_number() == metal_atomic_number:
-            metal_atom_ids.append(atom.get_id())
+    properties = {
+        'metals': None,
+        'Ns': None,
+        'Cs': None,
+    }
+    for path in paths:
+        path_atom_ids = paths[path]
+        # Get mismatches.
+        path1a = (path_atom_ids[1], path_atom_ids[0])
+        path1b = (path_atom_ids[-1], path_atom_ids[0])
+        path2a = (path_atom_ids[1], path_atom_ids[2])
+        path2b = (path_atom_ids[-1], path_atom_ids[2])
+        p1a_d = get_atom_distance(face, path1a[0], path1a[1])
+        p1b_d = get_atom_distance(face, path1b[0], path1b[1])
+        p2a_d = get_atom_distance(face, path2a[0], path2a[1])
+        p2b_d = get_atom_distance(face, path2b[0], path2b[1])
+        mismatch1 = (abs(p1a_d-p1b_d)/max([p1a_d, p1b_d])) * 100
+        mismatch2 = (abs(p2a_d-p2b_d)/max([p2a_d, p2b_d])) * 100
 
-    # Get metal-metal distances - as neighbours.
-    metal_metal_distances = sorted(
-        [(
-            i,
-            j,
-            get_atom_distance(
-                molecule=face,
-                atom1_id=i,
-                atom2_id=j
-            ),
-        ) for i, j in combinations(metal_atom_ids, r=2)
-        ],
-        key=lambda a: a[2]
-    )
-    properties = {'mismatches': []}
-    for metal_atom_id in metal_atom_ids:
-        # Assume neighbour is two shortest distances.
-        # Save to dictionary -- atom_id: [dist1, dist2]
-        neigh_dists = [
-            i[2]
-            for i in metal_metal_distances if metal_atom_id in i[:2]
-        ][:2]
-        properties[metal_atom_id] = neigh_dists
-        mismatch = (
-            100 * ((neigh_dists[1] - neigh_dists[0]) / neigh_dists[0])
-        )
-        properties['mismatches'].append(mismatch)
+        properties[path] = (mismatch1, mismatch2)
 
     return properties
 
 
-def get_face_properties(face, face_name):
+def visualise_face(face, face_name, paths):
+    """
+    Plot a visualisation of the face.
+
+    """
+
+    fig, ax = plt.subplots(figsize=(5, 5))
+    plot_properties = {
+        'metals': {'c': 'orange', 's': 160},
+        'Cs': {'c': 'gray', 's': 120},
+        'Ns': {'c': 'skyblue', 's': 120},
+    }
+    string = ''
+    # Plot the properties of each path.
+    xmin = 100
+    xmax = -100
+    ymin = 100
+    ymax = -100
+    for path in paths:
+        path_atom_ids = paths[path]
+        # Get mismatches.
+        path1a = (path_atom_ids[1], path_atom_ids[0])
+        path1b = (path_atom_ids[-1], path_atom_ids[0])
+        path2a = (path_atom_ids[1], path_atom_ids[2])
+        path2b = (path_atom_ids[-1], path_atom_ids[2])
+        p1a_d = get_atom_distance(face, path1a[0], path1a[1])
+        p1b_d = get_atom_distance(face, path1b[0], path1b[1])
+        p2a_d = get_atom_distance(face, path2a[0], path2a[1])
+        p2b_d = get_atom_distance(face, path2b[0], path2b[1])
+        mismatch1 = (abs(p1a_d-p1b_d)/max([p1a_d, p1b_d])) * 100
+        mismatch2 = (abs(p2a_d-p2b_d)/max([p2a_d, p2b_d])) * 100
+
+        string += (
+            f'{path}: ({round(p1a_d, 2)}, {round(p1b_d, 2)}), '
+            f'({round(p2a_d, 2)}, {round(p2b_d, 2)}) '
+            f'AR: {round(mismatch1, 2)}%, {round(mismatch2, 2)}%\n'
+        )
+
+        # Plot paths.
+        for pp in [path1a, path1b, path2a, path2b]:
+            pos1 = tuple(face.get_atomic_positions(pp[0]))[0]
+            pos2 = tuple(face.get_atomic_positions(pp[1]))[0]
+            x1 = pos1[0]
+            y1 = pos1[1]
+            x2 = pos2[0]
+            y2 = pos2[1]
+            ax.plot(
+                [x1, x2], [y1, y2],
+                c=plot_properties[path]['c'],
+                lw=2
+            )
+        # Plot atom positions.
+        for i in face.get_atomic_positions(path_atom_ids):
+            ax.scatter(
+                i[0], i[1],
+                c=plot_properties[path]['c'],
+                s=plot_properties[path]['s'],
+                alpha=1.0,
+                edgecolor='k',
+            )
+            xmax = max([xmax, i[0]])
+            xmin = min([xmin, i[0]])
+            ymax = max([ymax, i[1]])
+            ymin = min([ymin, i[1]])
+
+    xmid = 0  # (xmax+xmin)/2
+    ymid = 0  # (ymax+ymin)/2
+    ax.text(xmid-5, ymid-3, string, fontsize=8)
+
+    # ax.tick_params(axis='both', which='major', labelsize=16)
+    # ax.set_xlabel(r'$x$ [$\mathrm{\AA}}$]', fontsize=16)
+    # ax.set_ylabel(r'$y$ [$\mathrm{\AA}}$]', fontsize=16)
+    ax.set_aspect(1)
+    ax.axis('off')
+    filename = f'{face_name}_viz.pdf'
+    fig.tight_layout()
+    fig.savefig(filename, dpi=720, bbox_inches='tight')
+    plt.close()
+
+
+def get_face_properties(face, face_name, paths):
 
     json_file = f'{face_name}_properties.json'
     if exists(json_file):
         with open(json_file, 'r') as f:
             data = json.load(f)
     else:
-        data = calculate_face_properties(face)
+        data = calculate_face_properties(face, paths)
         with open(json_file, 'w') as f:
             json.dump(data, f)
 
@@ -236,34 +305,69 @@ def get_face_properties(face, face_name):
 
 def plot_face_mismatches(data, name):
 
-    avg_mismatches = {}
+    avg_m_mismatches = {}
+    avg_n_mismatches = {}
+    avg_c_mismatches = {}
 
     for face in data:
-        avg_mismatch = np.average(data[face]['mismatches'])
-        avg_mismatches[face] = avg_mismatch
+        avg_m_mismatch = np.average(data[face]['metals'])
+        avg_n_mismatch = np.average(data[face]['Ns'])
+        avg_c_mismatch = np.average(data[face]['Cs'])
+        avg_m_mismatches[face] = avg_m_mismatch
+        avg_n_mismatches[face] = avg_n_mismatch
+        avg_c_mismatches[face] = avg_c_mismatch
 
     x_ticks = [int(i) for i in data]
     x_ticklabels = [i for i in data]
 
-    width = 0.9
+    # width = 0.9
     fig, ax = plt.subplots(figsize=(8, 5))
 
-    ax.bar(
-        x=[int(i) for i in avg_mismatches],
-        height=[avg_mismatches[i] for i in avg_mismatches],
-        width=width,
-        facecolor=colors_i_like()[4],
-        edgecolor='none',
+    ax.scatter(
+        x=[int(i) for i in avg_m_mismatches],
+        y=[avg_m_mismatches[i] for i in avg_m_mismatches],
+        # width=width,
+        facecolor='orange',
+        edgecolor='k',
+        # linewidth=2,
+        s=160,
+        marker='o',
         alpha=1,
+        label='M-M',
     )
+
+    ax.scatter(
+        x=[int(i) for i in avg_n_mismatches],
+        y=[avg_n_mismatches[i] for i in avg_n_mismatches],
+        # width=width,
+        facecolor='skyblue',
+        edgecolor='k',
+        # linewidth=2,
+        s=160,
+        marker='P',
+        alpha=1,
+        label='N-N',
+    )
+
+    # ax.bar(
+    #     x=[int(i) for i in avg_c_mismatches],
+    #     height=[avg_c_mismatches[i] for i in avg_c_mismatches],
+    #     width=width,
+    #     facecolor='none',
+    #     edgecolor='gray',
+    #     linewidth=2,
+    #     alpha=1,
+    #     label='C-C',
+    # )
 
     # Set number of ticks for x-axis
     ax.tick_params(axis='both', which='major', labelsize=16)
-    ax.set_ylabel('face-side mismatch [%]', fontsize=16)
-    ax.set_ylim(0, 200)
+    ax.set_ylabel('avg. face-side mismatch [%]', fontsize=16)
+    ax.set_ylim(0, 75)
     # Set number of ticks for x-axis
     ax.set_xticks(x_ticks)
     ax.set_xticklabels(x_ticklabels)
+    ax.legend(fontsize=16)
 
     fig.tight_layout()
     fig.savefig(
@@ -272,6 +376,81 @@ def plot_face_mismatches(data, name):
         bbox_inches='tight'
     )
     plt.close()
+
+
+def get_paths(face, face_name, metal_atomic_number=30):
+    """
+    Get the atom id paths to use to calculate the mismatch.
+
+    """
+
+    # bb id: iter of neighbouring bb ids defined in CubeFace.
+    # Match these with the expected coordinates for specific corners.
+    bb_id_neigh_order = [None, None, None, None]
+    # Also get metal atom ids.
+    metal_atom_ids = []
+    for atomi in face.get_atom_infos():
+        atom = atomi.get_atom()
+        if atom.get_atomic_number() == metal_atomic_number:
+            metal_atom_ids.append(atom.get_id())
+            bbid = atomi.get_building_block_id()
+            pos = tuple(face.get_atomic_positions(atom.get_id()))[0]
+            x, y, _ = pos
+            if x > 0:
+                if y > 0:
+                    bb_id_neigh_order[0] = bbid
+                else:
+                    bb_id_neigh_order[1] = bbid
+            else:
+                if y > 0:
+                    bb_id_neigh_order[3] = bbid
+                else:
+                    bb_id_neigh_order[2] = bbid
+
+    if None in bb_id_neigh_order:
+        print(bb_id_neigh_order)
+        raise ValueError(
+            f'BB ID neighbour search failed for {face_name}'
+        )
+
+    N_atom_ids = []
+    C_atom_ids = []
+    # N atom id: C atom id
+    NC_pairs = {}
+    smarts = '[#6X3]-[#7]=[#6X3H1]-[#6X3!H1]'
+    rdkit_mol = face.to_rdkit_mol()
+    query_ids = get_query_atom_ids(smarts, rdkit_mol)
+    for atom_ids in query_ids:
+        C_atom_ids.append(atom_ids[0])
+        N_atom_ids.append(atom_ids[1])
+        NC_pairs[atom_ids[1]] = atom_ids[0]
+    if len(N_atom_ids) != 4:
+        raise ValueError(f'too many matches found in {face_name}!')
+
+    # Atom ids that make the path around the face matching the bb id
+    # neighbour order.
+    paths = {
+        'metals': [0, 0, 0, 0],
+        'Cs': [0, 0, 0, 0],
+        'Ns': [0, 0, 0, 0],
+    }
+    for ai in face.get_atom_infos():
+        atom = ai.get_atom()
+        atom_id = atom.get_id()
+        bb_id = ai.get_building_block_id()
+        if atom_id in metal_atom_ids:
+            order_idx = bb_id_neigh_order.index(bb_id)
+            paths['metals'][order_idx] = atom_id
+
+        if atom_id in N_atom_ids:
+            order_idx = bb_id_neigh_order.index(bb_id)
+            paths['Ns'][order_idx] = atom_id
+
+    for i, N_id in enumerate(paths['Ns']):
+        paired_C = NC_pairs[N_id]
+        paths['Cs'][i] = paired_C
+
+    return paths
 
 
 def main():
@@ -375,12 +554,21 @@ def main():
                     f'max bond length of {face_name} is {max(all_bls)}'
                 )
 
+            # Get the paths to use in visualisation and calculation.
+            paths = get_paths(opt_face, face_name)
+
+            # For visualisation.
+            visualise_face(opt_face, face_name, paths)
 
             # Measure properties.
-            face_properties = get_face_properties(opt_face, face_name)
+            face_properties = get_face_properties(
+                opt_face, face_name, paths
+            )
             print(
                 f":: {face_name}: "
-                f"{np.average(face_properties['mismatches'])}"
+                f"{np.average(face_properties['metals'])}, "
+                f"{np.average(face_properties['Ns'])}, "
+                f"{np.average(face_properties['Cs'])}, "
             )
             lig_faces[face_t] = face_properties
         plot_face_mismatches(data=lig_faces, name=lig)
