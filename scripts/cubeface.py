35a46f635db0fce0f8fa13213c929b59c5032b29
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-03-22 23:24:07 +0000

    Refactor.

--- scripts/cubeface.py
+++ scripts/cubeface.py
@@ -11,7 +11,6 @@ Date Created: 25 Jan 2021
 
 """
 
-from itertools import combinations
 from scipy.spatial.distance import euclidean
 import numpy as np
 
@@ -20,9 +19,6 @@ from stk.utilities import (
     get_acute_vector,
     get_plane_normal,
 )
-
-
-from atools import get_atom_distance
 
 
 class _FaceVertex(stk.Vertex):
@@ -218,85 +214,6 @@ class _MetalVertex(_FaceVertex):
         return {
             fg_id: edge.get_id() for fg_id, edge in enumerate(edges)
         }
-
-
-class FaceBuildingBlock(stk.BuildingBlock):
-
-    def get_bromines(self):
-
-        bromine_atom_ids = [
-            list(fg.get_deleter_ids())
-            for fg in self.get_functional_groups()
-        ]
-
-        return bromine_atom_ids
-
-    def show_long_axis(self, long_axis, path):
-        string = stk.XyzWriter().to_string(self)
-
-        # Long axis is along y direction.
-        x_pos = [0, long_axis[0]]
-        y_pos = [0, long_axis[1]]
-        z_pos = [0, long_axis[2]]
-
-        for x, y, z in zip(x_pos, y_pos, z_pos):
-            string += f'Ar {x} {y} {z}\n'
-
-        string = string.split('\n')
-        string[0] = str(int(string[0]) + len(x_pos))
-        string = '\n'.join(string)
-
-        with open(path, 'w') as f:
-            f.write(string)
-
-    def get_long_axis(self):
-
-        atom_ids = self.get_bromines()
-        if len(atom_ids) != 4:
-            raise ValueError(
-                f'{self} has too many deleter bromines.'
-            )
-
-        atom_dists = sorted(
-            [
-                (idx1, idx2, get_atom_distance(
-                    self,
-                    idx1,
-                    idx2
-                ))
-                for idx1, idx2 in combinations(atom_ids, r=2)
-            ],
-            key=lambda a: a[2]
-        )
-
-        # Can assume the ordering of the binder atom distances:
-        # 0, 1: short vectors
-        # 2, 3: long vectors
-        # 4, 5: diagonal vectors
-        # This fails when the molecule is not sufficiently anisotropic,
-        # at which point it will not matter.
-        short_vector_fg_1 = (
-            atom_dists[0][0], atom_dists[0][1]
-        )
-        short_vector_fg_2 = (
-            (atom_dists[1][0], atom_dists[1][1])
-            if (
-                atom_dists[1][0] not in short_vector_fg_1 and
-                atom_dists[1][1] not in short_vector_fg_1
-            ) else
-            (atom_dists[2][0], atom_dists[2][1])
-        )
-
-        short_pair_1_centroid = self.get_centroid(
-            atom_ids=(i[0] for i in short_vector_fg_1),
-        )
-        short_pair_2_centroid = self.get_centroid(
-            atom_ids=(i[0] for i in short_vector_fg_2),
-        )
-
-        long_axis = short_pair_2_centroid - short_pair_1_centroid
-
-        return long_axis
 
 
 class _LinkerVertex(_FaceVertex):
---
+++ scripts/facebuildingblock.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Distributed under the terms of the MIT License.
+
+"""
+Module defining FaceBuildingBlock class.
+
+Author: Andrew Tarzia
+
+Date Created: 22 Mar 2021
+
+"""
+
+from itertools import combinations
+
+import stk
+
+from atools import get_atom_distance
+
+
+class FaceBuildingBlock(stk.BuildingBlock):
+
+    def get_bromines(self):
+
+        bromine_atom_ids = [
+            list(fg.get_deleter_ids())
+            for fg in self.get_functional_groups()
+        ]
+
+        return bromine_atom_ids
+
+    def show_long_axis(self, long_axis, path):
+        string = stk.XyzWriter().to_string(self)
+
+        # Long axis is along y direction.
+        x_pos = [0, long_axis[0]]
+        y_pos = [0, long_axis[1]]
+        z_pos = [0, long_axis[2]]
+
+        for x, y, z in zip(x_pos, y_pos, z_pos):
+            string += f'Ar {x} {y} {z}\n'
+
+        string = string.split('\n')
+        string[0] = str(int(string[0]) + len(x_pos))
+        string = '\n'.join(string)
+
+        with open(path, 'w') as f:
+            f.write(string)
+
+    def get_long_axis(self):
+
+        atom_ids = self.get_bromines()
+        if len(atom_ids) != 4:
+            raise ValueError(
+                f'{self} has too many deleter bromines.'
+            )
+
+        atom_dists = sorted(
+            [
+                (idx1, idx2, get_atom_distance(
+                    self,
+                    idx1,
+                    idx2
+                ))
+                for idx1, idx2 in combinations(atom_ids, r=2)
+            ],
+            key=lambda a: a[2]
+        )
+
+        # Can assume the ordering of the binder atom distances:
+        # 0, 1: short vectors
+        # 2, 3: long vectors
+        # 4, 5: diagonal vectors
+        # This fails when the molecule is not sufficiently anisotropic,
+        # at which point it will not matter.
+        short_vector_fg_1 = (
+            atom_dists[0][0], atom_dists[0][1]
+        )
+        short_vector_fg_2 = (
+            (atom_dists[1][0], atom_dists[1][1])
+            if (
+                atom_dists[1][0] not in short_vector_fg_1 and
+                atom_dists[1][1] not in short_vector_fg_1
+            ) else
+            (atom_dists[2][0], atom_dists[2][1])
+        )
+
+        short_pair_1_centroid = self.get_centroid(
+            atom_ids=(i[0] for i in short_vector_fg_1),
+        )
+        short_pair_2_centroid = self.get_centroid(
+            atom_ids=(i[0] for i in short_vector_fg_2),
+        )
+
+        long_axis = short_pair_2_centroid - short_pair_1_centroid
+
+        return long_axis
