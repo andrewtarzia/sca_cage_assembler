e143c0a20ffa7aadc39296d19048f54b8728c842
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-03-22 23:24:44 +0000

    Add HoCube specific ligand FG selection.

--- scripts/cage_set.py
+++ scripts/cage_set.py
@@ -22,6 +22,10 @@ from glob import glob
 import stk
 
 import atools
+from stk import (
+    get_acute_vector,
+    get_plane_normal,
+)
 import symmetries
 from utilities import (
     get_planar_conformer,
@@ -30,6 +34,7 @@ from utilities import (
 )
 from cage_building import available_topologies
 from cage import Cage
+from facebuildingblock import FaceBuildingBlock
 
 
 class CageSet:
@@ -696,6 +701,123 @@ class HoCube(CageSet):
 
     """
 
+    def _load_ligand(self, ligand_name, ligand_dir):
+        ligand = FaceBuildingBlock.init_from_file(
+            join(ligand_dir, f'{ligand_name}_opt.mol'),
+            functional_groups=[stk.BromoFactory()]
+        )
+
+        return ligand
+
+    def _get_ligand(self, type_name, ligand_dir):
+        prop = self.ligand_dicts[self.cage_set_dict[type_name]]
+        linker = self._load_ligand(
+            ligand_name=self.cage_set_dict[type_name],
+            ligand_dir=ligand_dir
+        )
+
+        def reorient_linker(molecule):
+            target_coords = (
+                np.array([1, 1, 0]),
+                np.array([1, -1, 0]),
+                np.array([-1, -1, 0]),
+                np.array([-1, 1, 0]),
+            )
+            centroid_pos = np.array([0, 0, 0])
+            molecule = molecule.with_centroid(
+                position=centroid_pos,
+                atom_ids=molecule.get_placer_ids(),
+            )
+
+            edge_centroid = sum(target_coords) / len(target_coords)
+            edge_normal = get_acute_vector(
+                reference=edge_centroid,
+                vector=get_plane_normal(
+                    points=np.array(target_coords),
+                ),
+            )
+
+            fg_bonder_centroid = molecule.get_centroid(
+                atom_ids=next(
+                    molecule.get_functional_groups()
+                ).get_placer_ids(),
+            )
+            edge_position = target_coords[0]
+            molecule = (
+                molecule.with_rotation_to_minimize_angle(
+                    start=fg_bonder_centroid - centroid_pos,
+                    target=edge_position - edge_centroid,
+                    axis=edge_normal,
+                    origin=centroid_pos,
+                )
+            )
+
+            # Flatten wrt to xy plane.
+            core_centroid = molecule.get_centroid(
+                atom_ids=molecule.get_core_atom_ids(),
+            )
+            normal = molecule.get_plane_normal(
+                atom_ids=molecule.get_placer_ids(),
+            )
+            normal = get_acute_vector(
+                reference=core_centroid - centroid_pos,
+                vector=normal,
+            )
+            molecule = molecule.with_rotation_between_vectors(
+                start=normal,
+                target=[0, 0, 1],
+                origin=centroid_pos,
+            )
+
+            # Align long axis of molecule (defined by deleter atoms) with
+            # y axis.
+            long_axis_vector = molecule.get_long_axis()
+            edge_centroid = sum(target_coords) / len(target_coords)
+            edge_normal = get_acute_vector(
+                reference=edge_centroid,
+                vector=get_plane_normal(
+                    points=np.array(target_coords),
+                ),
+            )
+            molecule = (
+                molecule.with_rotation_to_minimize_angle(
+                    start=long_axis_vector,
+                    target=[1, 0, 0],
+                    axis=edge_normal,
+                    origin=centroid_pos,
+                )
+            )
+            return molecule
+
+        temp_linker = reorient_linker(linker)
+
+        # Set functional group ordering based on long axis.
+        fg_centroids = tuple(
+            temp_linker.get_centroid(
+                atom_ids=fg.get_placer_ids(),
+            ) for fg in temp_linker.get_functional_groups()
+        )
+        plus_minus_fg_id = tuple(
+            i for i, cent in enumerate(fg_centroids)
+            if cent[0] > 0 and cent[1] < 0
+        )[0]
+        fg1_id = plus_minus_fg_id
+        fg2_id, fg3_id, fg4_id = tuple(
+            i
+            for i in range(temp_linker.get_num_functional_groups())
+            if i != fg1_id
+        )
+        new_fgs = tuple(temp_linker.get_functional_groups())
+        linker = temp_linker.with_functional_groups(
+            functional_groups=(
+                new_fgs[fg1_id],
+                new_fgs[fg2_id],
+                new_fgs[fg3_id],
+                new_fgs[fg4_id],
+            )
+        )
+        return prop, linker
+
     def define_cages_to_build(self, ligand_dir, complex_dir):
         """
         Defines the name and objects of all cages to build.
@@ -717,7 +839,7 @@ class HoCube(CageSet):
         # Get linker and dictionary.
         tet_prop, tet_linker = self._get_ligand(
             type_name='tetratopic',
-            ligand_dir=ligand_dir
+            ligand_dir=ligand_dir,
         )
 
         # Get topology function as object to be used in following list.
