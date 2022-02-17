31426d1779e89f18d5f117f6abdb964b23ba395b
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-02-17 10:36:26 +0000

    Refactor bb alignment.

--- scripts/cage_set.py
+++ scripts/cage_set.py
@@ -20,10 +20,6 @@ import numpy as np
 
 import stk
 
-from stk import (
-    get_acute_vector,
-    get_plane_normal,
-)
 from symmetries import (
     M4L4_Symmetry,
     M6L2L3_Symmetry,
@@ -35,6 +31,7 @@ from utilities import (
     get_planar_conformer,
     convert_symm_names,
     calculate_ideal_pore_size,
+    reorient_linker,
 )
 from cage_building import available_topologies
 from cage import Cage
@@ -748,79 +745,6 @@ class HoCube(CageSet):
             ligand_name=self.cage_set_dict[type_name],
             ligand_dir=ligand_dir
         )
-
-        def reorient_linker(molecule):
-            target_coords = (
-                np.array([1, 1, 0]),
-                np.array([1, -1, 0]),
-                np.array([-1, -1, 0]),
-                np.array([-1, 1, 0]),
-            )
-            centroid_pos = np.array([0, 0, 0])
-            molecule = molecule.with_centroid(
-                position=centroid_pos,
-                atom_ids=molecule.get_placer_ids(),
-            )
-
-            edge_centroid = sum(target_coords) / len(target_coords)
-            edge_normal = get_acute_vector(
-                reference=edge_centroid,
-                vector=get_plane_normal(
-                    points=np.array(target_coords),
-                ),
-            )
-
-            fg_bonder_centroid = molecule.get_centroid(
-                atom_ids=next(
-                    molecule.get_functional_groups()
-                ).get_placer_ids(),
-            )
-            edge_position = target_coords[0]
-            molecule = (
-                molecule.with_rotation_to_minimize_angle(
-                    start=fg_bonder_centroid - centroid_pos,
-                    target=edge_position - edge_centroid,
-                    axis=edge_normal,
-                    origin=centroid_pos,
-                )
-            )
-
-            # Flatten wrt to xy plane.
-            core_centroid = molecule.get_centroid(
-                atom_ids=molecule.get_core_atom_ids(),
-            )
-            normal = molecule.get_plane_normal(
-                atom_ids=molecule.get_placer_ids(),
-            )
-            normal = get_acute_vector(
-                reference=core_centroid - centroid_pos,
-                vector=normal,
-            )
-            molecule = molecule.with_rotation_between_vectors(
-                start=normal,
-                target=[0, 0, 1],
-                origin=centroid_pos,
-            )
-
-            # Align long axis of molecule (defined by deleter atoms) with
-            # y axis.
-            long_axis_vector = molecule.get_long_axis()
-            edge_centroid = sum(target_coords) / len(target_coords)
-            edge_normal = get_acute_vector(
-                reference=edge_centroid,
-                vector=get_plane_normal(
-                    points=np.array(target_coords),
-                ),
-            )
-            molecule = (
-                molecule.with_rotation_to_minimize_angle(
-                    start=long_axis_vector,
-                    target=[1, 0, 0],
-                    axis=edge_normal,
-                    origin=centroid_pos,
-                )
-            )
-            return molecule
 
         temp_linker = reorient_linker(linker)
 
--- scripts/utilities.py
+++ scripts/utilities.py
@@ -24,6 +24,10 @@ from pymatgen.analysis.local_env import (
 )
 from scipy.spatial.distance import euclidean
 import stk
+from stk import (
+    get_acute_vector,
+    get_plane_normal,
+)
 import stko
 
 import env_set
@@ -1747,3 +1751,77 @@ def build_conformers(mol, N, ETKDG_version=None):
 
     print(f'there are {molecule.GetNumConformers()} conformers')
     return cids, molecule
+
+
+def reorient_linker(molecule):
+    target_coords = (
+        np.array([1, 1, 0]),
+        np.array([1, -1, 0]),
+        np.array([-1, -1, 0]),
+        np.array([-1, 1, 0]),
+    )
+    centroid_pos = np.array([0, 0, 0])
+    molecule = molecule.with_centroid(
+        position=centroid_pos,
+        atom_ids=molecule.get_placer_ids(),
+    )
+
+    edge_centroid = sum(target_coords) / len(target_coords)
+    edge_normal = get_acute_vector(
+        reference=edge_centroid,
+        vector=get_plane_normal(
+            points=np.array(target_coords),
+        ),
+    )
+
+    fg_bonder_centroid = molecule.get_centroid(
+        atom_ids=next(
+            molecule.get_functional_groups()
+        ).get_placer_ids(),
+    )
+    edge_position = target_coords[0]
+    molecule = (
+        molecule.with_rotation_to_minimize_angle(
+            start=fg_bonder_centroid - centroid_pos,
+            target=edge_position - edge_centroid,
+            axis=edge_normal,
+            origin=centroid_pos,
+        )
+    )
+
+    # Flatten wrt to xy plane.
+    core_centroid = molecule.get_centroid(
+        atom_ids=molecule.get_core_atom_ids(),
+    )
+    normal = molecule.get_plane_normal(
+        atom_ids=molecule.get_placer_ids(),
+    )
+    normal = get_acute_vector(
+        reference=core_centroid - centroid_pos,
+        vector=normal,
+    )
+    molecule = molecule.with_rotation_between_vectors(
+        start=normal,
+        target=[0, 0, 1],
+        origin=centroid_pos,
+    )
+
+    # Align long axis of molecule (defined by deleter atoms) with
+    # y axis.
+    long_axis_vector = molecule.get_long_axis()
+    edge_centroid = sum(target_coords) / len(target_coords)
+    edge_normal = get_acute_vector(
+        reference=edge_centroid,
+        vector=get_plane_normal(
+            points=np.array(target_coords),
+        ),
+    )
+    molecule = (
+        molecule.with_rotation_to_minimize_angle(
+            start=long_axis_vector,
+            target=[1, 0, 0],
+            axis=edge_normal,
+            origin=centroid_pos,
+        )
+    )
+    return molecule
