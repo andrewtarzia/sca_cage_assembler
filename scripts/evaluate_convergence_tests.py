e07e59a8194eb75b8f370ac967a4a07cd3f40cc8
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-03-09 17:58:57 +0000

    WIP: Update plots.

--- scripts/evaluate_convergence_tests.py
+++ scripts/evaluate_convergence_tests.py
@@ -14,6 +14,7 @@ Date Created: 20 Jan 2022
 import re
 import sys
 import glob
+import numpy as np
 import os
 import matplotlib.pyplot as plt
 
@@ -26,6 +27,46 @@ def get_cp2k_energy(file):
             if search in line:
                 string = nums.search(line.rstrip()).group(0)
                 return float(string)
+
+
+def check_convergence(file):
+    nums = re.compile(r"[+-]?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?")
+
+    converge_line = False
+    with open(file, 'r') as f:
+        for line in f.readlines():
+            if '*** SCF run converged in' in line:
+                converge_line = True
+            if 'Electronic density on regular grids:' in line:
+                string = nums.search(line.rstrip()).group(0)
+                edense_on_grids = -1*float(string)
+            if 'Number of electrons:' in line:
+                string = nums.search(line.rstrip()).group(0)
+                electrons = float(string)
+
+    if np.isclose(edense_on_grids, electrons, atol=1E-1):
+        electron_density = True
+    else:
+        electron_density = False
+
+    if converge_line and electron_density:
+        return True
+    else:
+        return False
+
+
+def get_cp2k_forces(file):
+    search = 'SUM OF ATOMIC FORCES'
+    nums = re.compile(r"[+-]?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?")
+    with open(file, 'r') as f:
+        for line in f.readlines():
+            if search in line:
+                string = nums.search(line.rstrip()).group(0)
+                print(string)
+                raise SystemExit()
+                return float(string)
+
+    return None
 
 
 def main():
@@ -43,15 +84,15 @@ Usage: evaluate_convergence_tests.py dft_directory
         dft_directory = sys.argv[1]
 
     _figure_path = 'figures'
-    output_files = glob.glob(os.path.join(dft_directory, '*out'))
+    output_files = [
+        i for i in glob.glob(os.path.join(dft_directory, '*out'))
+        if 'slurm' not in i
+    ]
     print(len(output_files))
 
-    cutoffs = range(50, 501, 50)
-    rel_cutoffs = range(10, 101, 10)
-
     systems_data = {
-        'C_cl1_quad2_12_t': {},
-        'C_cl1_quad2_3_c2v': {},
+        'C_cl1_quad2_12_th2': {},
+        'C_cl1_quad2_3_th2': {},
         'C_cl1_quad2_8_s62': {},
     }
     for fi in output_files:
@@ -59,109 +100,107 @@ Usage: evaluate_convergence_tests.py dft_directory
         system = temp.split('_optc_')[0][1:]
         cutoff = int(temp.split('_optc_')[1].split('_')[0])
         rel_cutoff = int(temp.split('_optc_')[1].split('_')[1])
-        energy = get_cp2k_energy(fi)
-        systems_data[system][(cutoff, rel_cutoff)] = energy
-
-    for syst in systems_data:
-        sdata = systems_data[syst]
-        min_energy = min([
-            sdata[i] for i in sdata if sdata[i] is not None
-        ])
-        print(min_energy)
-        cu_xs = {i: [] for i in cutoffs}
-        cu_ys = {i: [] for i in cutoffs}
-        for ds in sdata:
-            ey_da = sdata[ds]
-            if ey_da is not None:
-                # ey_da = (sdata[ds] - min_energy)
-                cu_xs[ds[0]].append(ds[1])
-                cu_ys[ds[0]].append(ey_da)
-
-        fig, ax = plt.subplots(figsize=(8, 5))
-        for cu in cutoffs:
-            ax.scatter(
-                cu_xs[cu],
-                [i-min(cu_ys[cu]) for i in cu_ys[cu]],
-                label=f'{cu} Ry',
-                s=40,
+        # Check convergence
+        if check_convergence(fi):
+            energy = get_cp2k_energy(fi)
+            forces = get_cp2k_forces(fi)
+            systems_data[system][(cutoff, rel_cutoff)] = (
+                energy, forces
             )
+        else:
+            print(f'{fi} not converged.')
+            systems_data[system][(cutoff, rel_cutoff)] = None
 
-        # Set number of ticks for x-axis
-        ax.set_yscale('log')
-        ax.tick_params(axis='both', which='major', labelsize=16)
-        ax.set_xlabel(r'rel. cutoff []', fontsize=16)
-        ax.set_ylabel(r'rel. DFT energy [a.u.]', fontsize=16)
-        # ax.set_xlim(0, 2500)
-        ax.set_ylim(-0.1, 1)
-        ax.legend(fontsize=16)
-        fig.tight_layout()
-        fig.savefig(
-            os.path.join(_figure_path, f'{syst}_convergence.pdf'),
-            dpi=720,
-            bbox_inches='tight',
-        )
-
+    print('cutoff convergence:')
+    inital_rel_cutoff = 60
+    chosen_cutoff = 700
     for syst in systems_data:
         sdata = systems_data[syst]
         xs = []
         ys = []
         for ds in sdata:
-            ey_da = sdata[ds]
-            if ey_da is not None and ds[1] == 60:
+            ey_da = sdata[ds][0]
+            fo_da = sdata[ds][1]
+            if ey_da is not None and ds[1] == inital_rel_cutoff:
                 xs.append(ds[0])
                 ys.append(ey_da)
 
+        xys = sorted(zip(xs, ys), key=lambda pair: pair[0])
+
         fig, ax = plt.subplots(figsize=(8, 5))
-        ax.scatter(
-            xs,
-            [i-min(ys) for i in ys],
-            s=80,
+        fin_y = [i[1] for i in xys][-1]
+        ax.plot(
+            [i[0] for i in xys],
+            [(i[1]-fin_y)*2625.5 for i in xys],
+            lw=3,
+            marker='o',
+            markersize=12,
+            c='firebrick',
         )
-        for x, y in zip(xs, ys):
+        for x, y in xys:
             print(x, y)
+
+        ax.axvline(x=chosen_cutoff, c='k', lw=3)
 
         # Set number of ticks for x-axis
         ax.tick_params(axis='both', which='major', labelsize=16)
         ax.set_xlabel(r'cutoff [Ry]', fontsize=16)
-        ax.set_ylabel(r'rel. DFT energy [a.u.]', fontsize=16)
+        ax.set_ylabel(r'rel. DFT energy [kJ mol$^{-1}$]', fontsize=16)
         # ax.set_xlim(0, 2500)
-        # ax.set_ylim(-0.1, 1)
+        ax.set_ylim(-1, 1)
         fig.tight_layout()
         fig.savefig(
-            os.path.join(_figure_path, f'{syst}_at60rel.pdf'),
+            os.path.join(
+                _figure_path, f'{syst}_at{inital_rel_cutoff}rel.pdf'
+            ),
             dpi=720,
             bbox_inches='tight',
         )
 
-
+    raise SystemExit('plot forces too')
+    print('\nrel cutoff convergence:')
+    chosen_rel_cutoff = 60
     for syst in systems_data:
         sdata = systems_data[syst]
         xs = []
         ys = []
         for ds in sdata:
-            ey_da = sdata[ds]
-            if ey_da is not None and ds[0] == 400:
+            ey_da = sdata[ds][0]
+            fo_da = sdata[ds][1]
+            print(ey_da, fo_da, ds, syst)
+            if ey_da is not None and ds[0] == chosen_cutoff:
                 xs.append(ds[1])
                 ys.append(ey_da)
 
+        xys = sorted(zip(xs, ys), key=lambda pair: pair[0])
+
         fig, ax = plt.subplots(figsize=(8, 5))
-        ax.scatter(
-            xs,
-            [i-min(ys) for i in ys],
-            s=80,
+        fin_y = [i[1] for i in xys][-1]
+        ax.plot(
+            [i[0] for i in xys],
+            [(i[1]-fin_y)*2625.5 for i in xys],
+            lw=3,
+            marker='o',
+            markersize=12,
+            c='firebrick',
         )
-        for x, y in zip(xs, ys):
+        for x, y in xys:
             print(x, y)
+
+
+        ax.axvline(x=chosen_rel_cutoff, c='k', lw=3)
 
         # Set number of ticks for x-axis
         ax.tick_params(axis='both', which='major', labelsize=16)
         ax.set_xlabel(r'rel. cutoff []', fontsize=16)
         ax.set_ylabel(r'rel. DFT energy [a.u.]', fontsize=16)
         # ax.set_xlim(0, 2500)
-        # ax.set_ylim(-0.1, 1)
+        ax.set_ylim(-1, 1)
         fig.tight_layout()
         fig.savefig(
-            os.path.join(_figure_path, f'{syst}_at400ry.pdf'),
+            os.path.join(
+                _figure_path, f'{syst}_at{chosen_rel_cutff}ry.pdf'
+            ),
             dpi=720,
             bbox_inches='tight',
         )
