f6113f31c73941834e3061dfd9fd8c5f1ef6731d
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-06-14 17:17:23 +0200

    Add new plot.

--- scripts/plot_cube_vs_properties.py
+++ scripts/plot_cube_vs_properties.py
@@ -14,7 +14,11 @@ Date Created: 08 Nov 2020
 import os
 import pandas as pd
 import sys
+from scipy.spatial import ConvexHull
 import matplotlib.pyplot as plt
+import numpy as np
+
+from utilities import convert_symm_names
 
 
 def main():
@@ -43,6 +47,24 @@ def main():
     no_forms_x = []
     no_forms_ye = []
     no_forms_yangle = []
+
+    all_e_vs_x = {
+        'd2': {'d': [], 'c': 'k', 'm': 'o'},
+        # 'th1': {'d': [], 'c': 'r', 'm': 'D'},
+        'th2': {'d': [], 'c': 'r', 'm': 'X'},
+        # 'td': {'d': [], 'c': 'r', 'm': 'o'},
+        'tl': {'d': [], 'c': 'gold', 'm': 'P'},
+        # 's41': {'d': [], 'c': 'gold', 'm': 'X'},
+        # 's42': {'d': [], 'c': 'gold', 'm': 'D'},
+        # 's61': {'d': [], 'c': 'gray', 'm': 'X'},
+        's62': {'d': [], 'c': 'gray', 'm': 'D'},
+        # 'd31': {'d': [], 'c': 'skyblue', 'm': 'P'},
+        'd32': {'d': [], 'c': 'skyblue', 'm': 'o'},
+        # 'd31n': {'d': [], 'c': 'b', 'm': 'P'},
+        # 'd32n': {'d': [], 'c': 'b', 'm': 'o'},
+        # 'c2v': {'d': [], 'c': 'green', 'm': 'o'},
+        # 'c2h': {'d': [], 'c': 'green', 'm': 'X'},
+    }
     for i, row in all_cage_data.iterrows():
         if row['m_cube_shape'] is None:
             continue
@@ -51,6 +73,9 @@ def main():
         x = float(row['m_cube_shape'])
         ye = float(row['rellsesum'])
         yangle = float(row['maxintangledev'])
+
+        if row['symmetry'] in all_e_vs_x:
+            all_e_vs_x[row['symmetry']]['d'].append((x, ye))
 
         if int(row['outcome']) == 1:
             forms_x.append(x)
@@ -101,6 +126,58 @@ def main():
     fig.tight_layout()
     fig.savefig(
         os.path.join(_figure_path, f"shape_vs_energies.pdf"),
+        dpi=720,
+        bbox_inches='tight'
+    )
+    plt.close()
+
+    fig, ax = plt.subplots(figsize=(8, 5))
+    for symm in all_e_vs_x:
+        points = np.array((
+            [i[0] for i in all_e_vs_x[symm]['d']],
+            [i[1] for i in all_e_vs_x[symm]['d']],
+        )).T
+        hull = ConvexHull(points)
+        for i, simplex in enumerate(hull.simplices):
+            if i == 0:
+                label = convert_symm_names(symm)
+            else:
+                label = None
+            ax.plot(
+                points[simplex, 0],
+                points[simplex, 1],
+                c=all_e_vs_x[symm]['c'],
+                lw=1,
+                alpha=1.0,
+                label=label,
+            )
+
+    ax.scatter(
+        forms_x,
+        forms_ye,
+        c='none',
+        edgecolors='k',
+        marker='o',
+        alpha=1.0,
+        s=120,
+        rasterized=True,
+        label='forms',
+    )
+
+    # Set number of ticks for x-axis
+    ax.tick_params(axis='both', which='major', labelsize=16)
+    ax.set_xlabel('CU-8 cube measure', fontsize=16)
+    ax.set_ylabel(
+        r'rel. sum strain energy [kJmol$^{-1}$]',
+        fontsize=16,
+    )
+    ax.set_xlim((-0.1, 2))
+    # ax.set_ylim(yprops[col_name][1])
+    ax.legend(fontsize=16, ncol=3)
+
+    fig.tight_layout()
+    fig.savefig(
+        os.path.join(_figure_path, f"shape_vs_energies_col.pdf"),
         dpi=720,
         bbox_inches='tight'
     )
