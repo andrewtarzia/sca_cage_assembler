48136e3dcc27502da8bcdfc40089819d741f4637
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-07-02 17:48:56 +0200

    Minor.

--- scripts/setup_lse_dft.py
+++ scripts/setup_lse_dft.py
@@ -20,10 +20,11 @@ import dft_utilities
 
 
 def main():
-    if (not len(sys.argv) == 4):
+    if (not len(sys.argv) == 5):
         print(
             """
 Usage: setup_lse_dft.py dft_directory cage_directory xray_directory
+delete_directory
 
     dft_directory : (str)
         Directory to run dft from - will be created.
@@ -33,12 +34,17 @@ Usage: setup_lse_dft.py dft_directory cage_directory xray_directory
 
     xray_directory : (str)
         Directory with extracted-from-crystal ligands.
+
+    delete_directory : (str)
+        't' to delete dft_directory, other to not.
+
 """)
         sys.exit()
     else:
         dft_directory = sys.argv[1]
         cage_directory = sys.argv[2]
         xray_directory = sys.argv[3]
+        delete_directory = True if sys.argv[4] == 't' else False
 
     all_free_ligands = glob.glob(os.path.join(
         cage_directory, '*_sg*_opt.mol'
@@ -58,12 +64,13 @@ Usage: setup_lse_dft.py dft_directory cage_directory xray_directory
     print(len(all_xray_ligands))
 
     dft_directory = os.path.abspath(dft_directory)
-    if os.path.exists(dft_directory):
+    if os.path.exists(dft_directory) and delete_directory:
         input(
             f'sure you want to delete {dft_directory}? ctrl-C if not!'
         )
         shutil.rmtree(dft_directory)
-    os.mkdir(dft_directory)
+    if not os.path.exists(dft_directory):
+        os.mkdir(dft_directory)
 
     all_ligands = all_free_ligands+all_extr_ligands+all_xray_ligands
     all_input_file_names = []
