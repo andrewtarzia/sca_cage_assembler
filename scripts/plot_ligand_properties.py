4cc1b662e57e61ac1dd6f487486b4e67e76b484a
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-11-14 19:47:04 +0100

    Add plot by NN definition, not aspect difference.

--- scripts/plot_ligand_properties.py
+++ scripts/plot_ligand_properties.py
@@ -187,12 +187,189 @@ def plot_MM_vs_AR(json_files, candms, expts, full=False, short=False, grouped=Fa
                 'short120': {
                     'ids': [1, 2, 5],
                     'cnum': 1.2,
-                    'labl': '$\Theta_{\mathrm{short}} = 120 \degree$',
+                    'labl': r'$\theta_{\mathrm{short}} = 120 \degree$',
                 },
                 'short60': {
                     'ids': [3, 4, 6],
                     'cnum': 0.8,
-                    'labl': '$\Theta_{\mathrm{short}} = 60 \degree$',
+                    'labl': r'$\theta_{\mathrm{short}} = 60 \degree$',
+                },
+            }
+            for group in groups:
+                X = [i[0] for i in XY1 if i[2][0] in groups[group]['ids']]
+                Y = [i[1] for i in XY1 if i[2][0] in groups[group]['ids']]
+                name = [i[2] for i in XY1 if i[2][0] in groups[group]['ids']]
+                labs = groups[group]['labl']
+                ax.plot(
+                    X,
+                    Y,
+                    c=adjust_lightness(c, groups[group]['cnum']),
+                    # edgecolors='k',
+                    marker='o',
+                    alpha=1.0,
+                    markersize=8,
+                    linewidth=3,
+                    linestyle='-',
+                    label=f'{lab}: {labs}'
+                )
+
+                for x, y, s in zip(X, Y, name):
+                    ax.text(s=s[1], x=x, y=y, fontsize=16)
+            alpha = 0.4
+            marker = None
+        else:
+            alpha = 1
+            marker = m
+            X = [i[0] for i in XY1]
+            Y = [i[1] for i in XY1]
+            name = [i[2][1] for i in XY1]
+            ax.plot(
+                X,
+                Y,
+                c=c,
+                # edgecolors='k',
+                marker=marker,
+                alpha=alpha,
+                markersize=8,
+                linewidth=3,
+                linestyle=ls,
+                label=f'{lab}'
+            )
+            for x, y, s in zip(X, Y, name):
+                ax.text(s=s, x=x, y=y, fontsize=16)
+
+    # ax[0].tick_params(axis='both', which='major', labelsize=16)
+    ax.tick_params(axis='both', which='major', labelsize=16)
+    # ax[1].set_xlabel('aspect ratio [1:X]', fontsize=16)
+    # ax.set_xlabel(
+    #     r'aspect difference [$\mathrm{\AA}$]', fontsize=16
+    # )
+    ax.set_xlabel(
+        (
+            r'avg. $\Delta$N $\cdot\cdot\cdot$'
+            r'N as AR [$\mathrm{\AA}$]'
+        ),
+        fontsize=16,
+    )
+    # ax[0].set_ylabel(r'avg. mismatch [$\mathrm{\AA}$]', fontsize=16)
+    # ax.set_ylabel(r'mismatch [$\mathrm{\AA}$]', fontsize=16)
+    ax.set_ylabel(
+        (
+            r'$\Delta$Zn$^{\mathrm{II}}\cdot\cdot\cdot$'
+            r'Zn$^{\mathrm{II}}$ [$\mathrm{\AA}$]'
+        ),
+        fontsize=16,
+    )
+    # ax[0].set_xlim(0, 8)
+    ax.set_xlim(0, 7)
+    # ax[0].set_ylim(0, 7)
+    ax.set_ylim(ylim)
+    ax.legend(fontsize=16, ncol=ncol)
+    fig.savefig(
+        os.path.join(
+            _figure_path, f'all_ligand_MM_vs_AR{addon}{suffix}.pdf'
+        ),
+        dpi=720,
+        bbox_inches='tight'
+    )
+
+    plt.close()
+
+
+def plot_MM_vs_NN(json_files, candms, expts, full=False, short=False, grouped=False):
+    _figure_path = 'figures'
+
+    if full:
+        addon = '_full'
+        ignored_faces = []
+    else:
+        ignored_faces = ['iii', 'v']
+        addon = ''
+
+    if short:
+        dict_key = 'face_properties'
+        suffix = '_short'
+    else:
+        dict_key = 'face_long_properties'
+        suffix = ''
+
+    if grouped:
+        addon += '_grouped'
+        ignored_faces = ['iii', 'iv', 'v', 'vi', 'vii']
+        ncol = 2
+        ylim = (0, 6)
+    else:
+        ncol = 5
+        ylim = (0, 10)
+
+    fig, ax = plt.subplots(figsize=(8, 5))
+    stabs = {
+        'i': {'nn': [], 'stab2': [], 'stab1': [], 'name': []},
+        'ii': {'nn': [], 'stab2': [], 'stab1': [], 'name': []},
+        'iii': {'nn': [], 'stab2': [], 'stab1': [], 'name': []},
+        'iv': {'nn': [], 'stab2': [], 'stab1': [], 'name': []},
+        'v': {'nn': [], 'stab2': [], 'stab1': [], 'name': []},
+        'vi': {'nn': [], 'stab2': [], 'stab1': [], 'name': []},
+        'vii': {'nn': [], 'stab2': [], 'stab1': [], 'name': []},
+    }
+    for i in json_files:
+        cage_set = i.replace('_ligand_measures.json', '')
+        if cage_set not in expts:
+            continue
+        print(f'doing {cage_set}')
+        with open(i, 'r') as f:
+            cage_set_data = json.load(f)
+        lname = cage_set.replace('cl1_', '')
+        for face in cage_set_data[dict_key]:
+            stabs[face]['nn'].append(
+                cage_set_data[dict_key][face][3]
+            )
+            stabs[face]['stab1'].append(
+                cage_set_data[dict_key][face][0]
+            )
+            stabs[face]['stab2'].append(
+                cage_set_data[dict_key][face][1]
+            )
+            stabs[face]['name'].append(
+                (
+                    convert_lig_names_from_cage(lname, as_int=True),
+                    convert_lig_names_from_cage(lname, as_sub=True)
+                )
+            )
+
+    for face in stabs:
+        if face in ignored_faces:
+            continue
+        c, m, lab = candms[face]
+
+        XY1 = [
+            (y, x, name) for y, x, name
+            in sorted(
+                zip(
+                    stabs[face]['nn'],
+                    stabs[face]['stab1'],
+                    stabs[face]['name'],
+                ),
+                key=lambda pair: pair[0]
+            )
+        ]
+
+        if face in ['i', 'ii']:
+            ls = '-'
+        else:
+            ls = 'dashed'
+
+        if grouped:
+            groups = {
+                'short120': {
+                    'ids': [1, 2, 5],
+                    'cnum': 1.2,
+                    'labl': r'$\theta_{\mathrm{short}} = 120 \degree$',
+                },
+                'short60': {
+                    'ids': [3, 4, 6],
+                    'cnum': 0.8,
+                    'labl': r'$\theta_{\mathrm{short}} = 60 \degree$',
                 },
             }
             for group in groups:
@@ -267,7 +444,7 @@ def plot_MM_vs_AR(json_files, candms, expts, full=False, short=False, grouped=Fa
     ax.legend(fontsize=16, ncol=ncol)
     fig.savefig(
         os.path.join(
-            _figure_path, f'all_ligand_MM_vs_AR{addon}{suffix}.pdf'
+            _figure_path, f'all_ligand_MM_vs_NN{addon}{suffix}.pdf'
         ),
         dpi=720,
         bbox_inches='tight'
@@ -318,6 +495,9 @@ def main():
     plot_MM_vs_AR(json_files, candms, experimental_results, short=True)
     plot_MM_vs_AR(json_files, candms, experimental_results, full=True)
     plot_MM_vs_AR(json_files, candms, experimental_results, grouped=True)
+
+    plot_MM_vs_NN(json_files, candms, experimental_results)
+    plot_MM_vs_NN(json_files, candms, experimental_results, grouped=True)
 
 
 if __name__ == "__main__":
