f840eeb6621dad48dd90ad77335dd78574f3c36c
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-11-15 14:30:15 +0100

    Remove unused.

--- scripts/cage_building.py
+++ scripts/cage_building.py
@@ -13,46 +13,10 @@ Date Created: 03 Mar 2020
 
 import stk
 
-from knot_topology import M8L6KnotPrism
-
-
-def m4l4spacer_graph(building_blocks, vertex_alignments):
-
-    topology_graph = stk.cage.M4L4Tetrahedron(
-        building_blocks=building_blocks,
-        vertex_alignments=vertex_alignments,
-        num_processes=2,
-    )
-
-    return topology_graph
-
 
 def m8l6_graph(building_blocks, vertex_alignments):
 
     topology_graph = stk.cage.M8L6Cube(
-        building_blocks=building_blocks,
-        vertex_alignments=vertex_alignments,
-        num_processes=2,
-    )
-
-    return topology_graph
-
-
-def m8l6knot_graph(building_blocks, vertex_alignments):
-
-    topology_graph = M8L6KnotPrism(
-        building_blocks=building_blocks,
-        vertex_alignments=vertex_alignments,
-        num_processes=2,
-        optimizer=stk.MCHammer(),
-    )
-
-    return topology_graph
-
-
-def m6l2l3_graph(building_blocks, vertex_alignments):
-
-    topology_graph = stk.cage.M6L2L3Prism(
         building_blocks=building_blocks,
         vertex_alignments=vertex_alignments,
         num_processes=2,
@@ -68,10 +32,7 @@ def available_topologies(string):
     """
 
     topologies = {
-        'm4l4spacer': m4l4spacer_graph,
         'm8l6face': m8l6_graph,
-        'm6l2l3': m6l2l3_graph,
-        'm8l6knot': m8l6knot_graph,
     }
 
     try:
